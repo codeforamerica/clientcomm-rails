ad9c11a73a92b416b2dae25712fca41864543fae
Thomas Apodaca <tma@angrymonkey.com>
2017-05-10 16:39:38 -0700

    failing test for visiting the index page

---
+++ spec/acceptance/user_visits_clients_page_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+feature "user visits clients page" do
+    scenario "successfully" do
+        visit root_path
+
+        expect(page).to have_css 'h1', text: 'Clients'
+    end
+end
