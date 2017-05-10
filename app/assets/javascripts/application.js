7e89fb3b11ae34eccddaa1886604c143059152cc
Thomas Apodaca <tma@angrymonkey.com>
2017-05-10 14:58:56 -0700

    reformat application javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,33 +14,29 @@
 //= require jquery_ujs
 //= require_tree .
 
-
 var incrementer = (function() {
   var i = {
     increment: function(input) {
       var max = parseInt($(input).attr('max'));
       var value = parseInt($(input).val());
-      if(max != undefined) {
-        if(value < max) {
-          $(input).val(value+1);
+      if (max !== undefined) {
+        if (value < max) {
+          $(input).val(value + 1);
         }
-      }
-      else {
-        $(input).val(parseInt($(input).val())+1);
+      } else {
+        $(input).val(parseInt($(input).val()) + 1);
       }
     },
     decrement: function(input) {
       var min = parseInt($(input).attr('min'));
       var value = parseInt($(input).val());
-      if(min != undefined) {
-        if(value > min) {
-          $(input).val(value-1);
+      if (min !== undefined) {
+        if (value > min) {
+          $(input).val(value - 1);
         }
+      } else {
+        $(input).val(value - 1);
       }
-      else {
-        $(input).val(value-1);
-      }
-
     },
     init: function() {
       $('.incrementer').each(function(index, incrementer) {
@@ -57,93 +53,113 @@ var incrementer = (function() {
         });
       });
     }
-  }
+  };
   return {
     init: i.init
-  }
+  };
 })();
 
 var radioSelector = (function() {
   var rs = {
     init: function() {
-      $('.radio-button').each(function(index, button){
-        if($(this).find('input').is(':checked')) {
+      $('.radio-button').each(function(index, button) {
+        if ($(this).find('input').is(':checked')) {
           $(this).addClass('is-selected');
         }
 
         $(this).find('input').click(function(e) {
           $(this).parent().siblings().removeClass('is-selected');
           $(this).parent().addClass('is-selected');
-        })
-      })
+        });
+      });
     }
-  }
+  };
   return {
     init: rs.init
-  }
+  };
 })();
 
 var checkboxSelector = (function() {
   var cs = {
     init: function() {
-      $('.checkbox').each(function(index, button){
-        if($(this).find('input').is(':checked')) {
+      $('.checkbox').each(function(index, button) {
+        if ($(this).find('input').is(':checked')) {
           $(this).addClass('is-selected');
         }
 
         $(this).find('input').click(function(e) {
-          if($(this).is(':checked')) {
+          if ($(this).is(':checked')) {
             $(this).parent().addClass('is-selected');
-          }
-          else {
+          } else {
             $(this).parent().removeClass('is-selected');
           }
-        })
-      })
+        });
+      });
     }
-  }
+  };
   return {
     init: cs.init
-  }
+  };
 })();
 
 var followUpQuestion = (function() {
   var fUQ = {
     init: function() {
-
-      // if any initial questions are already selected on page load, show the follow up
-      $('.question-with-follow-up__question').find('.radio-button, .checkbox').each(function(index, button) {
-        if($(this).find('.question-with-follow-up__question input').is(':checked') && $(this).attr('data-follow-up') != null) {
-          $($(this).attr('data-follow-up')).show();
-        }
-      });
-
-      $('.question-with-follow-up').each(function(index, question) {
-        var self = this;
-
-        // if any initial questions are already selected on page load, show the follow up
-        $(self).find('.question-with-follow-up__question').find('.radio-button, .checkbox').each(function(index, button) {
-          if($(this).find('input').is(':checked') && $(this).attr('data-follow-up') != null) {
+      // if any initial questions are already selected on page load,
+      // show the follow up
+      $('.question-with-follow-up__question')
+        .find('.radio-button, .checkbox')
+        .each(function(index, button) {
+          if (
+            $(this)
+              .find('.question-with-follow-up__question input')
+              .is(':checked') &&
+            $(this).attr('data-follow-up') !== null
+          ) {
             $($(this).attr('data-follow-up')).show();
           }
         });
 
-        // add click listeners to initial question inputs
-        $(self).find('.question-with-follow-up__question input').click(function(e) {
-          // reset follow ups
-          $(self).find('.question-with-follow-up__follow-up input').attr('checked', false);
-          $(self).find('.question-with-follow-up__follow-up').find('.radio-button, .checkbox').removeClass('is-selected');
-          $(self).find('.question-with-follow-up__follow-up').hide();
+      $('.question-with-follow-up').each(function(index, question) {
+        var self = this;
 
-          // show the current follow up
-          $($(this).attr('data-follow-up')).show();
-        })
+        // if any initial questions are already selected on page load,
+        // show the follow up
+        $(self)
+          .find('.question-with-follow-up__question')
+          .find('.radio-button, .checkbox')
+          .each(function(index, button) {
+            if (
+              $(this).find('input').is(':checked') &&
+              $(this).attr('data-follow-up') !== null
+            ) {
+              $($(this).attr('data-follow-up')).show();
+            }
+          });
+
+        // add click listeners to initial question inputs
+        $(self)
+          .find('.question-with-follow-up__question input')
+          .click(function(e) {
+            // reset follow ups
+            $(self)
+              .find('.question-with-follow-up__follow-up input')
+              .attr('checked', false);
+            $(self)
+              .find('.question-with-follow-up__follow-up')
+              .find('.radio-button, .checkbox')
+              .removeClass('is-selected');
+            $(self).find('.question-with-follow-up__follow-up').hide();
+
+            // show the current follow up
+            $($(this).attr('data-follow-up')).show();
+          });
       });
     }
-  }
+  };
   return {
     init: fUQ.init
-  }
+  };
 })();
 
 $(document).ready(function() {
@@ -152,5 +168,3 @@ $(document).ready(function() {
   checkboxSelector.init();
   followUpQuestion.init();
 });
-
-
