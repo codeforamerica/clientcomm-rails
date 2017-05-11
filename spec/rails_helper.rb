c8fe7b53fdab563d6f6c76046505948b20f40d63
Thomas Apodaca <tma@angrymonkey.com>
2017-05-11 00:55:19 -0700

    copy settings from gcf

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,8 +5,10 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'webmock/rspec'
+require 'capybara/rails'
 
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 ActiveRecord::Migration.maintain_test_schema!
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,9 @@
 RSpec.configure do |config|
+  unless ENV['CI']
+    config.run_all_when_everything_filtered = true
+    config.filter_run focus: true
+  end
+
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
@@ -9,17 +14,26 @@ RSpec.configure do |config|
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
-  config.filter_run_when_matching :focus
-  config.example_status_persistence_file_path = "spec/examples.txt"
-  config.disable_monkey_patching!
-
   if config.files_to_run.one?
     config.default_formatter = "doc"
   end
 
-  config.profile_examples = 10
-
   config.order = :random
 
-  Kernel.srand config.seed
+  config.before(:each) do
+    # Stub all posts to the mixpanel analytics API to respond with a 200 success
+    stub_request(:post, /api.mixpanel.com/)
+      .with(headers: { 'Accept' => '*/*', 'User-Agent' => 'Ruby' })
+      .to_return(status: 200, body: "stubbed response", headers: {})
+  end
+
+  config.after(:each) do
+    # Prevents any test doubles for this singleton class from being re-used
+    # across examples, which is not supported.
+    AnalyticsService.instance_variable_set(:@singleton__instance__, nil)
+  end
+
+  config.after(:all) do
+    I18n.locale = :en
+  end
 end
