7da54c1adbb8371593aba0a7ea75ea1b85b58571
Thomas Apodaca <tma@angrymonkey.com>
2017-05-15 20:28:02 -0700

    add devise setup

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -17,4 +17,8 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
+  # Devise setup
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
+  config.include Warden::Test::Helpers
 end
