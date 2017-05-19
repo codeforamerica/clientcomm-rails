c132a0021645177e8cb246893a1303aa97017696
Thomas Apodaca <tma@angrymonkey.com>
2017-05-19 14:04:02 -0700

    generate the messages controller and add links

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file,
+  # see http://guides.rubyonrails.org/routing.html
+
   # USERS
   devise_for :users
 
   # CLIENTS
   root to: "clients#index"
   resources :clients, only: [:index, :new, :create]
+
+  # MESSAGES
+  resources :clients do
+    resources :messages, only: [:index]
+  end
+  resources :messages, only: [:create]
 
   # TWILIO
   post '/incoming/sms', to: 'twilio#incoming_sms'
