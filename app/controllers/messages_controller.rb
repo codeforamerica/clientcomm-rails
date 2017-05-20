125ac8f3561f6efcc9945c8865f3890ace76f648
Thomas Apodaca <tma@angrymonkey.com>
2017-05-19 17:15:26 -0700

    very rough first pass at message page content

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,6 @@
 class MessagesController < ApplicationController
   def index
     @messages = current_user.messages.where(client_id: params["client_id"]).order('created_at ASC')
+    @client = Client.find params[:client_id]
   end
 end
