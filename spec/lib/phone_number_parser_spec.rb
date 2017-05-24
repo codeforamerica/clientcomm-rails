0df1b525ddf08af59c52c6996e782ab9137e7bef
Thomas Apodaca <tma@angrymonkey.com>
2017-05-24 10:48:55 -0700

    using rspec magic on phone number parser tests
    As suggested by @tdooner.

--- spec/lib/phone_number_parser_spec.rb
+++ spec/lib/phone_number_parser_spec.rb
@@ -1,23 +1,33 @@
-require "rails_helper"
+require 'rails_helper'
 
 describe PhoneNumberParser do
-  it "adds country code to a bare number" do
-    input_number = "2435551212"
-    expect(described_class.normalize(input_number)).to eq "+12435551212"
+
+  describe '#normalize' do
+    subject { described_class.normalize(input) }
+
+    context 'adds country code to a bare number' do
+      let(:input) { '2435551212' }
+      it { expect(subject).to eq '+12435551212' }
+    end
+
+    context 'normalizes a number with non-numeric characters' do
+      let(:input) { '(243) 555-1212' }
+      it { expect(subject).to eq '+12435551212' }
+    end
   end
 
-  it "normalizes a number with non-numeric characters" do
-    input_number = "(243) 555-1212"
-    expect(described_class.normalize(input_number)).to eq "+12435551212"
+  describe '#format_for_display' do
+    subject { described_class.format_for_display(input) }
+
+    context 'formats a normalized number for display' do
+      let(:input) { '+12435551212' }
+      it { expect(subject).to eq '(243) 555-1212' }
+    end
+
+    context 'formats a bare number for display' do
+      let(:input) { '2435551212' }
+      it { expect(subject).to eq '(243) 555-1212' }
+    end
   end
 
-  it "formats a normalized number for display" do
-    input_number = "+12435551212"
-    expect(described_class.format_for_display(input_number)).to eq "(243) 555-1212"
-  end
-
-  it "formats a bare number for display" do
-    input_number = "2435551212"
-    expect(described_class.format_for_display(input_number)).to eq "(243) 555-1212"
-  end
 end
