57aa8b5c0466f37e6da916667f9f52c38408ba8b
Thomas Apodaca <tma@angrymonkey.com>
2017-05-24 01:45:50 -0700

    handle incoming SMSs

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -2,12 +2,18 @@ class TwilioController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def incoming_sms
-
+    # TODO: error handling
+    # get the client based on the phone number
+    client = Client.find_by(phone_number: params[:From])
+    # create a new message
+    new_message_params = {client: client, user_id: client.user_id, number_to: ENV['TWILIO_PHONE_NUMBER'], number_from: params[:From], inbound: true, twilio_sid: params[:SmsSid], twilio_status: params[:SmsStatus], body: params[:Body]}
+    Message.create(new_message_params)
+    # TODO: return 200
   end
 
   def incoming_sms_status
-    # update the status of the corresponding message in the database
     # TODO: error handling
+    # update the status of the corresponding message in the database
     message = Message.find_by twilio_sid: params[:SmsSid]
     message.update(twilio_status: params[:SmsStatus])
   end
