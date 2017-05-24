afa1a78b1d6814838aeda707ce8883f88c09d9c0
Thomas Apodaca <tma@angrymonkey.com>
2017-05-24 12:21:09 -0700

    handle phone_number normalization in the model
    With tests. As suggested by @tdooner

--- app/models/client.rb
+++ app/models/client.rb
@@ -6,4 +6,9 @@ class Client < ApplicationRecord
     "#{first_name} #{last_name}"
   end
 
+  # override default accessors
+  def phone_number=(number_input)
+    self[:phone_number] = PhoneNumberParser.normalize(number_input)
+  end
+
 end
