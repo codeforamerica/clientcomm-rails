279b131ccdeab1cd236f4f620b24eec221f542fc
Thomas Apodaca <tma@angrymonkey.com>
2017-05-25 17:19:14 -0700

    action cable enabled live messaging MVP
    JS to clear form and scroll page to bottom when a new message is added
    to the list, restyling the submit button so it can be disabled while
    the message is sending, passing client id via a data parameter instead
    of grabbing it from the URL

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -2,21 +2,26 @@
 //= require_self
 //= require_tree .
 
+$(document).ready(function() {
+  messages = $('#message-list');
+  clientId = messages.data('client-id');
+  messages_to_bottom = function() {
+    return $(document).scrollTop(messages.prop('scrollHeight'));
+  };
 
-const match = window.location.pathname.match(/\/clients\/(\d)+\/messages/);
-const clientId = match && match[1];
+  App.messages = App.cable.subscriptions.create(
+    { channel: 'MessagesChannel', client_id: clientId },
+    {
+      received: function(data) {
+        return this.renderMessage(data.message);
+      },
 
-if (clientId) {
-  App.messages = App.cable.subscriptions.create({ channel: 'MessagesChannel', client_id: clientId }, {
-    received: function(data) {
-      return this.renderMessage(data.message);
-    },
-
-    renderMessage: function(message) {
-      $.ajax(`/messages/${message.id}`)
-        .done(function (data) {
+      renderMessage: function(message) {
+        $.ajax(`/messages/${message.id}`).done(function(data) {
           $('#message-list').append(data);
+          messages_to_bottom();
         });
+      }
     }
-  });
-}
+  );
+});
---
+++ app/assets/javascripts/messages.js
@@ -0,0 +1,5 @@
+
+$(document).on('submit', '#new_message', function(e) {
+  // clear the message body text field
+  $('#message_body').val('');
+});
