83bd7b6d49aab82f5a88f74c036130221388c017
Thomas Apodaca <tma@angrymonkey.com>
2017-06-06 22:18:07 -0700

    expand & improve clients controller tests

--- spec/controllers/clients_controller_spec.rb
+++ spec/controllers/clients_controller_spec.rb
@@ -2,21 +2,34 @@ require 'rails_helper'
 
 RSpec.describe ClientsController, type: :controller do
   describe '#index' do
-    it 'requires authenticated user' do
+    it 'rejects unauthenticated user' do
       get :index
+      expect(response.code).to eq '302'
       expect(response).to redirect_to new_user_session_path
     end
 
-    it 'returns user clients' do
+    it 'allows authenicated user' do
       user = create :user
-      create :client, :user => user, :first_name => 'Alice'
-      create :client, :user => user, :first_name => 'Bob'
-      usertwo = create :user
-      create :client, :user => usertwo, :first_name => 'Stan'
       sign_in user
       get :index
-      expect(response.code).to eq "200"
-      expect(assigns[:clients].map(&:first_name)).to match_array %w[Alice Bob]
+      expect(response.code).to eq '200'
+      expect(response).to render_template 'index'
+    end
+
+    it "returns only authenticated user's clients" do
+      # one user with two clients
+      userone = create :user
+      create :client, :user => userone, :first_name => 'Elísa'
+      create :client, :user => userone, :first_name => 'Helena'
+      # another user with one client
+      usertwo = create :user
+      create :client, :user => usertwo, :first_name => 'Unnsteinn'
+      # sign in as the first user
+      sign_in userone
+      get :index
+      expect(response.code).to eq '200'
+      # should only see the first user's clients
+      expect(assigns[:clients].map(&:first_name)).to match_array %w[Elísa Helena]
     end
 
   end
