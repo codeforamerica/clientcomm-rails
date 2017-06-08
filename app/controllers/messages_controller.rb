7b0936b4d6b2c5c4aaa4170e924741a1b83ad949
Thomas Apodaca <tma@angrymonkey.com>
2017-06-08 16:43:10 -0700

    messages marked read when user visits index

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,6 +7,7 @@ class MessagesController < ApplicationController
     @client = Client.find params[:client_id]
     # the list of past messages
     @messages = current_user.messages.where(client_id: params["client_id"]).order('created_at ASC')
+    @messages.update_all(read: true)
     # a new message for the form
     @message = Message.new
   end
