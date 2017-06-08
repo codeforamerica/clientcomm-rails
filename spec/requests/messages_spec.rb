949bc9a46b1caf07bc90759e9620c48bc5524ef3
Thomas Apodaca <tma@angrymonkey.com>
2017-06-08 15:33:42 -0700

    failing test for marking messages read

---
+++ spec/requests/messages_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'Messages', type: :request do
+  context 'GET#index' do
+    it 'marks all messages read when index loaded' do
+      user = create :user
+      sign_in user
+      client = build :client, user: user
+      create_client client
+      message = create :message, user: user, client: client, inbound: true
+
+      # when we visit the messages path, it should mark the message read
+      expect { get client_messages_path(client) }
+        .to change { message.reload.read? }
+        .from(false)
+        .to(true)
+    end
+  end
+end
