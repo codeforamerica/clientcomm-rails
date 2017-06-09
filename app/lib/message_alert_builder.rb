8457af0b14d8a16953b97fc3c1e6a76fa1c2b5a9
Thomas Apodaca <tma@angrymonkey.com>
2017-06-08 17:19:30 -0700

    flesh out alert builder tests and functionality

--- app/lib/message_alert_builder.rb
+++ app/lib/message_alert_builder.rb
@@ -1,6 +1,7 @@
 
 class MessageAlertBuilder
   include Rails.application.routes.url_helpers
+  include ActionView::Helpers::TextHelper
 
   def build(user:)
     # return an alert appropriate for the state of unread messages
@@ -12,18 +13,18 @@ class MessageAlertBuilder
       lookup = user.messages.group(:client).count
       if lookup.length == 1
         client = lookup.keys.first
+        message_count = lookup.values.first
         {
-          text: "You have an unread message from #{client.full_name}",
+          text: "You have #{pluralize(message_count, 'unread message')} from #{client.full_name}",
           link_to: client_messages_path(client)
         }
       else
-        nil
+        message_count = lookup.values.sum
+        {
+          text: "You have #{pluralize(message_count, 'unread message')}",
+          link_to: clients_path
+        }
       end
     end
-
-    # "You have an unread message from Donald Duck"
-    # "You have 5 unread messages from Donald Duck"
-    # "You have 12 unread messages"
-
   end
 end
--- spec/lib/message_alert_builder_spec.rb
+++ spec/lib/message_alert_builder_spec.rb
@@ -8,21 +8,45 @@ describe MessageAlertBuilder do
       expect(described_class.new.build(user: user)).to eq nil
     end
 
+    specify "when there are unread messages for a different user's clients" do
+      userone = create :user
+      usertwo = create :user
+      client = create :client, user: userone, first_name: "Donald", last_name: "Duck"
+      create :message, user: userone, client: client, inbound: true, read: false
+      expect(described_class.new.build(user: usertwo)).to eq nil
+    end
+
     specify 'when there is one unread message' do
       user = create :user
       client = create :client, user: user, first_name: "Donald", last_name: "Duck"
       create :message, user: user, client: client, inbound: true, read: false
       expect(described_class.new.build(user: user)).to eq({
-        text: "You have an unread message from Donald Duck",
+        text: "You have 1 unread message from Donald Duck",
         link_to: "/clients/#{client.id}/messages"
       })
     end
 
     specify 'when there is more than one unread message from the same client' do
-
+      user = create :user
+      client = create :client, user: user, first_name: "Donald", last_name: "Duck"
+      create :message, user: user, client: client, inbound: true, read: false
+      create :message, user: user, client: client, inbound: true, read: false
+      expect(described_class.new.build(user: user)).to eq({
+        text: "You have 2 unread messages from Donald Duck",
+        link_to: "/clients/#{client.id}/messages"
+      })
     end
 
     specify 'when there is more than one unread message from multiple clients' do
+      user = create :user
+      clientone = create :client, user: user, first_name: "Donald", last_name: "Duck"
+      clienttwo = create :client, user: user, first_name: "Donald", last_name: "Duck"
+      create :message, user: user, client: clientone, inbound: true, read: false
+      create :message, user: user, client: clienttwo, inbound: true, read: false
+      expect(described_class.new.build(user: user)).to eq({
+        text: "You have 2 unread messages",
+        link_to: "/clients"
+      })
     end
   end
 end
