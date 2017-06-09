f460cbe3085e64d9b216c8739e8e50cdb7115581
Thomas Apodaca <tma@angrymonkey.com>
2017-06-08 17:03:24 -0700

    let read in the message factory default to false

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -6,7 +6,6 @@ FactoryGirl.define do
     sequence(:number_from) { |n| "243" + (n.to_s + (1000000 + Random.rand(10000000 - 1000000)).to_s)[0..6] }
     sequence(:number_to) { |n| "244" + (n.to_s + (1000000 + Random.rand(10000000 - 1000000)).to_s)[0..6] }
     inbound {[true, false].sample}
-    read {[true, false].sample}
     sequence(:twilio_sid) { |n| (n.to_s + SecureRandom.hex(17))[0..33] }
     twilio_status {["queued", "sending", "sent", "failed", "received"].sample}
   end
