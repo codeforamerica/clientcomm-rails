232da9affa3b6e79b9b22494475957b803e081e4
Thomas Apodaca <tma@angrymonkey.com>
2017-06-13 16:28:23 -0700

    expanded helper for analytics events testing

--- spec/support/analytics_helper.rb
+++ spec/support/analytics_helper.rb
@@ -1,5 +1,17 @@
 module AnalyticsHelper
   def expect_analytics_events(*events)
-    expect(@mixpanel_event_names).to eq events 
+    if events.all? { |event| event.is_a? String }
+      # it's an array of strings
+      expect(@mixpanel_event_names).to eq events 
+    else
+      # it's an array of hashes
+      events.each do |event_description|
+        event_name = event_description.keys.first
+        event_properties = event_description.values.first
+        found_request = @mixpanel_requests.find { |req| req.has_key? event_name }
+        fail "Could not find #{event_name} in the requests" unless found_request
+        expect(found_request[event_name]).to include event_properties
+      end
+    end
   end
 end
