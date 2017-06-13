335b85247a97f3309de108f13307dae9d1abc11b
Thomas Apodaca <tma@angrymonkey.com>
2017-06-12 21:25:54 -0700

    test on visible elements instead of db contents

--- spec/features/user_receives_new_message_marked_read_spec.rb
+++ spec/features/user_receives_new_message_marked_read_spec.rb
@@ -11,16 +11,17 @@ feature "User receives a message from a client" do
   context "while on the client's messages page" do
     it "marks the message as read", :js do
       # go to the messages page
-      myclient_id = Client.find_by(phone_number: PhoneNumberParser.normalize(clientone.phone_number)).id
-      visit client_messages_path(client_id: myclient_id)
+      clientone_phone = PhoneNumberParser.normalize(clientone.phone_number)
+      clientone_id = Client.find_by(phone_number: clientone_phone).id
+      visit client_messages_path(client_id: clientone_id)
       # post a message to the twilio endpoint from the user
       twilio_post_sms(twilio_new_message_params(clientone.phone_number))
       # there's a message with the correct contents
       expect(page).to have_css '.message--inbound p', text: twilio_message_text
-      # there's only one message in the database, and it's marked read
-      all_messages = Message.all
-      expect(all_messages.length).to eq 1
-      expect(all_messages.first.read).to eq true
+      # load the message index
+      visit clients_path
+      # the client isn't marked as having unread messages
+      expect(page).to have_css '.read td', text: clientone.full_name
     end
   end
 end
