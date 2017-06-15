482686ffbee4328af7475d0ef5713d8121a1cfb6
Thomas Apodaca <tma@angrymonkey.com>
2017-06-15 09:17:01 -0700

    breaking out the IP pieces a little more
    With the idea that I'll store some of the values somewhere instead of
    building them on every request.

--- app/helpers/analytics_helper.rb
+++ app/helpers/analytics_helper.rb
@@ -30,8 +30,11 @@ module AnalyticsHelper
   end
 
   def id_prefix
+    URI.parse(id_base).hostname.split(".")[0..1].join("_")
+  end
+
+  def id_base
     # NOTE: DEPLOY_BASE_URL is set by heroku
-    base = request.base_url || ENV['DEPLOY_BASE_URL']
-    URI.parse(base).hostname.split(".")[0..1].join("_")
+    request.base_url || ENV['DEPLOY_BASE_URL']
   end
 end
