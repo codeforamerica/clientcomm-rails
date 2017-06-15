ec31c7c4880c4f0e0f28a37e87875db36348d440
Thomas Apodaca <tma@angrymonkey.com>
2017-06-15 08:57:31 -0700

    add user id and ip to analytics tracking

--- app/helpers/analytics_helper.rb
+++ app/helpers/analytics_helper.rb
@@ -5,18 +5,31 @@ module AnalyticsHelper
   def analytics_track(label:, data: {})
     # NOTE: May eventually want to diverge distinct and visitor IDs, so
     #       tracking them separately for now.
-    distinct_id = session[:visitor_id]
+
     tracking_data = data.merge(
-      source: session[:source],
+      ip: visitor_ip,
       visitor_id: session[:visitor_id]
     )
     user_agent = request.env['HTTP_USER_AGENT']
 
     AnalyticsService.instance.track(
-      distinct_id: distinct_id.to_s,
+      distinct_id: distinct_id,
       label: label,
       user_agent: user_agent,
       data: tracking_data
     )
+  end
+
+  def visitor_ip
+    request.remote_ip || request.env['HTTP_X_FORWARDED_FOR']
+  end
+
+  def distinct_id
+    distinct = !current_user.nil? ? current_user.id : session[:visitor_id]
+    "#{id_prefix}-#{distinct}"
+  end
+
+  def id_prefix
+    URI.parse(request.base_url).hostname.split(".")[0..1].join("_")
   end
 end
