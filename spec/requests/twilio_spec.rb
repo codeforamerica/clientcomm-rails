6c06eafd56f05265137fd000fc7b835fb026bd34
Thomas Apodaca <tma@angrymonkey.com>
2017-06-20 14:28:13 -0700

    failing test for message attachments

---
+++ spec/requests/twilio_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe 'Twilio controller', type: :request do
+  context 'POST#incoming_sms' do
+    it 'receives an incoming sms message' do
+      user = create :user
+      sign_in user
+      clientone = create_client build(:client)
+      # post a new message
+      message_text = 'Hello, this is a new message from a client!'
+      message_params = twilio_new_message_params(
+        clientone.phone_number, nil, message_text
+      )
+      twilio_post_sms message_params
+      msg = user.messages.last
+      expect(msg).not_to eq nil
+      expect(msg.body).to eq message_text
+    end
+
+    it 'receives an incoming sms message with an attachment' do
+      user = create :user
+      sign_in user
+      clientone = create_client build(:client)
+      # post a new message
+      message_text = 'Hello, this is a new message from a client!'
+      message_params = twilio_new_message_params(
+        clientone.phone_number, nil, message_text, 2
+      )
+      twilio_post_sms message_params
+      msg = user.messages.last
+      expect(msg).not_to eq nil
+      expect(msg.body).to eq message_text
+      atts = msg.attachments.all
+      expect(atts.length).to eq 2
+    end
+  end
+end
+
