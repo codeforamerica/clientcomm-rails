a126a3258526de9327a8e4e9499bf49bffdb18fa
Thomas Apodaca <tma@angrymonkey.com>
2017-06-21 16:35:09 -0700

    updated tests to look for divs instead of ps

--- spec/features/user_gets_notification_when_message_received_spec.rb
+++ spec/features/user_gets_notification_when_message_received_spec.rb
@@ -50,7 +50,7 @@ feature "User receives a message from a client" do
       # post a message to the twilio endpoint from the user
       twilio_post_sms(twilio_new_message_params(from_number: clientone.phone_number))
       # there's a message with the correct contents
-      expect(page).to have_css '.message--inbound p', text: twilio_message_text
+      expect(page).to have_css '.message--inbound div', text: twilio_message_text
       # there's no flash notification
       expect(page).to have_no_css '.flash'
     end
