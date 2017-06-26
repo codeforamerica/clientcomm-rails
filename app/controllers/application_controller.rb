caed19af6e651f501bcfdab90c42f2e205b5668c
Thomas Apodaca <tma@angrymonkey.com>
2017-06-26 16:47:54 -0700

    before_filter is deprecated, use before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_visitor_id
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_filter :set_phone_number
+  before_action :set_phone_number
 
   private
 
