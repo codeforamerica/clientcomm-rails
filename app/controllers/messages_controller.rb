aa9ffd4a53f0eb3b352cbd915b4701a670e01133
Paras Sanghavi <paras@codeforamerica.org>
2017-06-27 21:05:50 -0700

    Remove unused route for messages/create with html format
     * Fix tests to use js format
     * Inline gcf_form_builder.gcf_send_message_field
     * remove outer send button label to fix failing test
     * Move messages analytics testing into a controller test

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -53,7 +53,6 @@ class MessagesController < ApplicationController
     )
 
     respond_to do |format|
-      format.html { redirect_to client_messages_path(client.id) }
       format.js { head :no_content }
     end
   end
