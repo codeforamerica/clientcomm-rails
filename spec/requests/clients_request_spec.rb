c563c3460514a42ca5d43d59a82b24aef13f77ed
Paras Sanghavi <paras@codeforamerica.org>
2017-07-06 11:02:34 -0700

    Move authentication testing to one test per controller

--- spec/requests/clients_request_spec.rb
+++ spec/requests/clients_request_spec.rb
@@ -1,56 +1,23 @@
 require 'rails_helper'
 
 describe 'Access to clients methods', type: :request do
-  context 'GET#index' do
+  context 'unauthenticated' do
     it 'rejects unauthenticated user' do
       get clients_path
       expect(response.code).to eq '302'
       expect(response).to redirect_to new_user_session_path
     end
-
-    it 'allows authenticated user' do
-      user = create :user
-      sign_in user
-      get clients_path
-      expect(response.code).to eq '200'
-    end
   end
 
-  context 'GET#new' do
-    it 'rejects unauthenticated user' do
-      get new_client_path
-      expect(response.code).to eq '302'
-      expect(response).to redirect_to new_user_session_path
-    end
+  context 'authenticated' do
+    let(:user) { create :user }
 
-    it 'allows authenticated user' do
-      user = create :user
+    before do
       sign_in user
-      get new_client_path
-      expect(response.code).to eq '200'
-    end
-  end
-
-  context 'POST#create' do
-    context 'unauthenticated user' do
-      let(:client) {build(:client)}
-
-      before do
-        create_client client
-      end
-
-      it 'rejects unauthenticated user' do
-        expect(response.code).to eq '302'
-        expect(response).to redirect_to new_user_session_path
-        expect(Client.count).to eq 0
-      end
     end
 
-    context 'authenticated user' do
+    context 'POST#create' do
       before do
-        user = create :user
-        sign_in user
-
         create_client client
       end
 
