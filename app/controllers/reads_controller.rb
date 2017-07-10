e10724be9d9506b80fb9c639b59c5d3a7c65b20a
Tomas Apodaca <tma@angrymonkey.com>
2017-07-10 01:04:05 -0700

    move message find into method, require params

--- app/controllers/reads_controller.rb
+++ app/controllers/reads_controller.rb
@@ -4,7 +4,6 @@ class ReadsController < ApplicationController
 
   def create
     # change the read status of the message
-    message = current_user.messages.find params[:message_id]
     success = message.update_attributes read: message_params[:read]
     if success
       head :no_content
@@ -13,8 +12,14 @@ class ReadsController < ApplicationController
     end
   end
 
+  private
+
+  def message
+    current_user.messages.find params[:message_id]
+  end
+
   def message_params
-    params.fetch(:message, {})
+    params.require(:message)
       .permit(:read)
   end
 end
