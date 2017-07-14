e6f3609769095195b361d220d2e978a6678f101f
Tomas Apodaca <tma@angrymonkey.com>
2017-07-14 13:54:44 -0700

    adding default url to routes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,6 +69,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: ENV['DEPLOY_BASE_URL'] }
+  routes.default_url_options = { host: ENV['DEPLOY_BASE_URL'] }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
