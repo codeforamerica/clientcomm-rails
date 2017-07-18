2c325cc2db25cae09675ab63b45fbb6ee1917692
Tomas Apodaca <tma@angrymonkey.com>
2017-07-18 16:11:33 -0700

    use example.com instead of test.com

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(email: 'test@test.com', password: 'changeme')
+User.create!(email: 'test@example.com', password: 'changeme')
