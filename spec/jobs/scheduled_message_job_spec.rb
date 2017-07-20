030b7f6aedbcfc8eea439992b991b4ba70411e2a
Tomas Apodaca <tomas@codeforamerica.org>
2017-07-20 11:28:03 -0700

    Add back in accidentally removed test logic
    Signed-off-by: Zak Auerbach <zauerbach@codeforamerica.org>

--- spec/jobs/scheduled_message_job_spec.rb
+++ spec/jobs/scheduled_message_job_spec.rb
@@ -2,10 +2,25 @@ require 'rails_helper'
 
 describe ScheduledMessageJob, active_job: true, type: :job do
   let(:message){ create :message }
+  let(:count) { 4 }
+  let(:link_html) { 'scheduled_messages_link_partial' }
+  let(:scheduled_messages) { double('scheduled_messages', count: count) }
+
   subject(:scheduled_job){ ScheduledMessageJob.perform_later(message: message, callback_url: 'whocares.com') }
 
   it 'calls SMSService when performed' do
     expect(SMSService.instance).to receive(:send_message).with(message: message, callback_url: 'whocares.com')
+    expect_any_instance_of(ScheduledMessageJob).to receive(:scheduled_messages)
+      .with(user: message.user)
+      .and_return(scheduled_messages)
+
+    expect(MessagesController).to receive(:render)
+      .with(partial: 'messages/scheduled_messages_link', locals: {count: count})
+      .and_return(link_html)
+
+    expect(ActionCable.server).to receive(:broadcast)
+      .with("scheduled_messages_#{message.client.id}", link_html: link_html, count: count)
+
     perform_enqueued_jobs { scheduled_job }
   end
 end
