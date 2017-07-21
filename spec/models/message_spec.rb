0b2745023d123c2ef3ecce17b615114ae0c27f58
Paras Sanghavi <paras@codeforamerica.org>
2017-07-21 11:27:33 -0700

    Restructure message_spec   * Use shoulda matchers for relationships

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,48 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  let!(:user) { create :user }
-  let!(:client) { create :client, :user => user }
-  let!(:message) { create :message, :user => user, :client => client}
-
-  describe 'relationship to client' do
-    it 'belongs to a client' do
-      expect(message.client).to eq(client)
-    end
-  end
-
-  describe 'relationship to user' do
-    it 'belings to a user' do
-      expect(message.user).to eq(user)
+  describe 'relationships' do
+    it do
+      should belong_to :client
+      should belong_to :user
+      should have_many :attachments
     end
   end
 
   describe '#create_from_twilio' do
-    it 'errors if no client matches the passed phone number' do
-      params = twilio_new_message_params from_number: '+99999999999'
-      expect {
-        Message.create_from_twilio!(params)
-      }.to raise_exception ActiveRecord::RecordNotFound
+    let(:user) { create :user }
+    let(:client) { create :client, user: user }
+
+    context 'client does not exist' do
+      it 'errors' do
+        params = twilio_new_message_params from_number: '+99999999999'
+        expect {
+          Message.create_from_twilio!(params)
+        }.to raise_exception ActiveRecord::RecordNotFound
+      end
     end
 
-    it 'creates a message if proper params are sent' do
-      params = twilio_new_message_params from_number: client.phone_number
-      msg = Message.create_from_twilio!(params)
-      expect(client.messages.last).to eq msg
-    end
+    context 'client exists' do
+      it 'creates a message if proper params are sent' do
+        params = twilio_new_message_params from_number: client.phone_number
+        msg = Message.create_from_twilio!(params)
+        expect(client.messages.last).to eq msg
+      end
 
-    it 'creates a message with attachments' do
-      params = twilio_new_message_params(
-        from_number: client.phone_number, media_count: 2
-      )
-      msg = Message.create_from_twilio!(params)
-      expect(msg).not_to eq nil
-      atts = msg.attachments.all
-      expect(atts.length).to eq 2
-      urls = atts.map(&:url)
-      types = atts.map(&:content_type)
-      expect(urls).to match_array(params.fetch_values('MediaUrl0', 'MediaUrl1'))
-      expect(types).to match_array(params.fetch_values('MediaContentType0', 'MediaContentType1'))
+      it 'creates a message with attachments' do
+        params = twilio_new_message_params(
+            from_number: client.phone_number, media_count: 2
+        )
+        msg = Message.create_from_twilio!(params)
+        expect(msg).not_to eq nil
+
+        attachments = msg.attachments.all
+        expect(attachments.length).to eq 2
+
+        urls = attachments.map(&:url)
+        expect(urls).to match_array(params.fetch_values('MediaUrl0', 'MediaUrl1'))
+
+        types = attachments.map(&:content_type)
+        expect(types).to match_array(params.fetch_values('MediaContentType0', 'MediaContentType1'))
+      end
     end
   end
 
