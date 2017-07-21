3793e221fc3ee707ace8f234ca19b39f63a38271
Paras Sanghavi <paras@codeforamerica.org>
2017-07-21 10:44:23 -0700

    Combine twilio request specs into one file
     * de-duplicate some test setup

--- spec/requests/twilio_spec.rb
+++ spec/requests/twilio_spec.rb
@@ -1,59 +1,95 @@
 require 'rails_helper'
 
 describe 'Twilio controller', type: :request do
+  let(:user) { create :user }
+  let(:client) { create(:client, user: user) }
+
+  before do
+    sign_in user
+  end
+
   context 'POST#incoming_sms' do
-    it 'saves an incoming sms message' do
-      user = create :user
-      sign_in user
-      clientone = create_client build(:client)
-      # post a new message
-      message_text = 'Hello, this is a new message from a client!'
-      message_params = twilio_new_message_params(
-        from_number: clientone.phone_number, msg_txt: message_text
+    let(:message_text) { 'Hello, this is a new message from a client!' }
+    let(:message_params) {
+      twilio_new_message_params(
+          from_number: client.phone_number,
+          msg_txt: message_text
       )
+    }
+
+    before do
       twilio_post_sms message_params
+    end
+
+    it 'saves an incoming sms message' do
       msg = user.messages.last
       expect(msg).not_to eq nil
       expect(msg.body).to eq message_text
+    end
+
+    it 'tracks an incoming sms message' do
+      expect_analytics_events(
+          {
+              'message_receive' => {
+                  'client_id' => client.id,
+                  'message_length' => message_text.length,
+                  'attachments_count' => 0
+              }
+          }
+      )
+    end
+
+    context 'sms message contains an attachment' do
+      let(:message_params) {
+        twilio_new_message_params(
+            from_number: client.phone_number,
+            msg_txt: message_text,
+            media_count: 1
+        )
+      }
+
+      it 'tracks an analytics event for the attachment' do
+        expect_analytics_events(
+            {
+                'message_receive' => {
+                    'client_id' => client.id,
+                    'message_length' => message_text.length,
+                    'attachments_count' => 1
+                }
+            }
+        )
+      end
     end
   end
 
   context 'POST#incoming_sms_status' do
     it 'saves a successful sms message status update' do
-      user = create :user
-      sign_in user
-      clientone = create_client build(:client)
-      msgone = create :message, user: user, client: clientone, inbound: true, twilio_status: 'queued'
+      msgone = create :message, user: user, client: client, inbound: true, twilio_status: 'queued'
       # validate the status
-      expect(clientone.messages.last.twilio_status).to eq 'queued'
+      expect(client.messages.last.twilio_status).to eq 'queued'
 
       # post a status update
-      status_params = twilio_status_update_params from_number: clientone.phone_number, sms_sid: msgone.twilio_sid, sms_status: 'received'
+      status_params = twilio_status_update_params from_number: client.phone_number, sms_sid: msgone.twilio_sid, sms_status: 'received'
       twilio_post_sms_status status_params
 
       # validate the updated status
-      expect(clientone.messages.last.twilio_status).to eq 'received'
+      expect(client.messages.last.twilio_status).to eq 'received'
 
       # no failed analytics event
       expect_analytics_events_not_happened('message_send_failed')
     end
-  end
 
-  context 'POST#incoming_sms_status' do
     it 'saves an unsuccessful sms message status update' do
-      user = create :user
-      sign_in user
-      clientone = create_client build(:client)
-      msgone = create :message, user: user, client: clientone, inbound: true, twilio_status: 'queued'
+      msgone = create :message, user: user, client: client, inbound: true, twilio_status: 'queued'
       # validate the status
-      expect(clientone.messages.last.twilio_status).to eq 'queued'
+      expect(client.messages.last.twilio_status).to eq 'queued'
 
       # post a status update
-      status_params = twilio_status_update_params from_number: clientone.phone_number, sms_sid: msgone.twilio_sid, sms_status: 'failed'
+      status_params = twilio_status_update_params from_number: client.phone_number, sms_sid: msgone.twilio_sid, sms_status: 'failed'
       twilio_post_sms_status status_params
 
       # validate the updated status
-      expect(clientone.messages.last.twilio_status).to eq 'failed'
+      expect(client.messages.last.twilio_status).to eq 'failed'
 
       # failed analytics event
       expect_analytics_events_happened('message_send_failed')
