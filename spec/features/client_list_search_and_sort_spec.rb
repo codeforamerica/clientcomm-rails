8307a93faf75ff2cf32290d6fc83dbc5181a7197
Charlie Weems <charlie.weems@gmail.com>
2017-07-24 11:24:36 -0700

    Consolidate search spec user/client setup

--- spec/features/client_list_search_and_sort_spec.rb
+++ spec/features/client_list_search_and_sort_spec.rb
@@ -1,17 +1,18 @@
 require "rails_helper"
 
-feature "search clients" do
+feature "search and sort clients" do
+  let!(:myuser) { create :user }
+  let!(:clientone) { create :client, user: myuser}
+  let!(:clienttwo) { create :client, user: myuser}
+  let!(:clientthree) { create :client, user: myuser}
+
+  before do
+    login_as(myuser, :scope => :user)
+    visit clients_path
+  end
+
   describe "user searches by name" do
     it "filters client list to match input", js: true do
-      myuser = create :user
-      clientone = create :client, user: myuser
-      clienttwo = create :client, user: myuser
-      clientthree = create :client, user: myuser
-
-      login_as(myuser, :scope => :user)
-
-      visit clients_path
-
       expect(page).to have_css '.data-table td', text: clientone.full_name
       expect(page).to have_css '.data-table td', text: clienttwo.full_name
       expect(page).to have_css '.data-table td', text: clientthree.full_name
@@ -25,15 +26,6 @@ feature "search clients" do
     end
 
     it "shows all results when clear search button is clicked", js: true do
-      myuser = create :user
-      clientone = create :client, user: myuser
-      clienttwo = create :client, user: myuser
-      clientthree = create :client, user: myuser
-
-      login_as(myuser, :scope => :user)
-
-      visit clients_path
-
       expect(page).to have_css '#clear_search'
 
       fill_in "Search clients by name", with: clientone.full_name
@@ -51,11 +43,9 @@ feature "search clients" do
     end
   end
 
-end
-
-feature "sort clients" do
-#  it "page is sorted by most recent contact by default" do
-#    expect(page).to have_selector('.glyphicon-arrow-down', visible: true)
-#  end
-
+  describe "user sorts clients" do
+    it "page is sorted by most recent contact by default", js: true do
+      expect(page).to have_css('.glyphicon-arrow-down')
+    end
+  end
 end
