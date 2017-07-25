be05fc9ecab04bd48180879f1fc56319815f7a9c
Charlie Weems <charlie.weems@gmail.com>
2017-07-24 18:08:41 -0700

    Working no search results message

--- app/assets/javascripts/client_search.js
+++ app/assets/javascripts/client_search.js
@@ -5,6 +5,14 @@ $(document).ready(function(){
 
   var clientList = new List('client-list', clientSeachOptions);
 
+  clientList.on('updated', function(list){
+    if(list.matchingItems.length === 0){
+      $('#no-search-results').show();
+    } else {
+      $('#no-search-results').hide();
+    }
+  });
+
   $('#clear_search').click(function(){
     $('.search').val('');
     clientList.search();
