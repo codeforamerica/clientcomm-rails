4a3549ee907e3c0dd2aa5d14e5b426ee8ba9bd78
Paras Sanghavi <paras@codeforamerica.org>
2017-07-31 15:59:23 -0700

    Add message attachments to email notifications
    * Plain text just says there's an attachment
    
    [#149255457]
    
    Signed-off-by: Charlie Weems <charlie@codeforamerica.org>

--- spec/mailers/notification_mailer_spec.rb
+++ spec/mailers/notification_mailer_spec.rb
@@ -3,8 +3,9 @@ require "rails_helper"
 describe NotificationMailer, type: :mailer do
   describe '#message_notification' do
     let(:user) {build(:user)}
-    let(:client) {build(:client, id: 4)}
-    let(:message) {create(:message, client: client, created_at: Time.zone.local(2012, 07, 11, 20, 10, 0))}
+    let(:client) {build(:client, id: 123456789)}
+    let(:attachment) {build :attachment}
+    let(:message) {create(:message, client: client, created_at: Time.zone.local(2012, 07, 11, 20, 10, 0), attachments: [attachment])}
     let(:mail) {NotificationMailer.message_notification(user, message)}
 
     shared_examples_for 'notification email' do
@@ -26,12 +27,20 @@ describe NotificationMailer, type: :mailer do
       subject {mail.body.encoded}
 
       it_behaves_like 'notification email'
+
+      it 'renders the attachment' do
+        expect(subject).to include(attachment.url)
+      end
     end
 
     context 'text part' do
       subject {mail.text_part.body}
 
       it_behaves_like 'notification email'
+
+      it 'shows a message for an attachment' do
+        expect(subject).to include '[Image attached, view on ClientComm]'
+      end
     end
   end
 end
--- spec/mailers/previews/notification_mailer_preview.rb
+++ spec/mailers/previews/notification_mailer_preview.rb
@@ -1,9 +1,9 @@
 # Preview all emails at http://localhost:3000/rails/mailers/notification_mailer
 class NotificationMailerPreview < ActionMailer::Preview
   def message_notification
-    user = User.first
-    client = user.clients.first
     message = Message.where(client: client, user: user).first
+    # message = Message.find(Attachment.first.message_id)
+    user = message.user
     NotificationMailer.message_notification(user, message)
   end
 end
