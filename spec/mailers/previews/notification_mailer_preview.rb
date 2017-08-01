cdd9a23cfbd133e67b2a2bfe1585770ec9938560
Paras Sanghavi <paras@codeforamerica.org>
2017-08-01 15:37:48 -0700

    Remove quotation marks in email notifications if there is not body text
    [#149255457]
    
    Signed-off-by: Charlie Weems <charlie@codeforamerica.org>

--- spec/mailers/previews/notification_mailer_preview.rb
+++ spec/mailers/previews/notification_mailer_preview.rb
@@ -1,7 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/notification_mailer
 class NotificationMailerPreview < ActionMailer::Preview
   def message_notification
-    message = Message.where(client: client, user: user).first
+    message = Message.first
     # message = Message.find(Attachment.first.message_id)
     user = message.user
     NotificationMailer.message_notification(user, message)
