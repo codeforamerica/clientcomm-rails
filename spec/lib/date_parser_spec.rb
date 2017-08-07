b07cace1fb29a16f959e9c051d29aeaef0fff862
Paras Sanghavi <paras@codeforamerica.org>
2017-08-07 14:56:46 -0700

    Add tests for nil date and time [#149315501]
    Signed-off-by: Charlie Weems <charlie@codeforamerica.org>

--- spec/lib/date_parser_spec.rb
+++ spec/lib/date_parser_spec.rb
@@ -38,4 +38,22 @@ describe DateParser do
       expect(subject).to eq Time.zone.local(2002, 12, 03, 12, 30)
     end
   end
+
+  context 'with valid date and nil time' do
+    let(:date) {'12/03/2002'}
+    let(:time) { nil }
+
+    it 'returns valid date' do
+      expect(subject).to eq nil
+    end
+  end
+
+  context 'with valid time and nil date' do
+    let(:date) { nil }
+    let(:time) { '3:30pm' }
+
+    it 'returns valid date' do
+      expect(subject).to eq nil
+    end
+  end
 end
