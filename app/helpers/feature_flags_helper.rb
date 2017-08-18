443900ad1f15238ec506d804f8cab634bd5139c0
Charlie Weems <charlie@codeforamerica.org>
2017-08-18 14:13:54 -0700

    Remove schedule messages feature flag
    [#150268859]

--- app/helpers/feature_flags_helper.rb
+++ app/helpers/feature_flags_helper.rb
@@ -1,7 +1,4 @@
 module FeatureFlagsHelper
   class FeatureFlags
-    def scheduled_messages
-      ENV['SCHEDULED_MESSAGES'] == 'true'
-    end
   end
 end
--- spec/features/feature_flags_spec.rb
+++ spec/features/feature_flags_spec.rb
@@ -1,41 +1,6 @@
 require 'rails_helper'
 
 feature 'feature flags' do
-  describe 'scheduled messages' do
-    let(:user) { create :user }
-    let(:client) { create(:client, user: user) }
-
-    before do
-      login_as(user, :scope => :user)
-      @scheduled_messages_value = ENV['SCHEDULED_MESSAGES']
-    end
-
-    after do
-      ENV['SCHEDULED_MESSAGES'] = @scheduled_messages_value
-    end
-
-    context 'enabled' do
-      before do
-        ENV['SCHEDULED_MESSAGES'] = 'true'
-        visit client_messages_path(client)
-      end
-
-      it 'shows the send later button' do
-        expect(page).to have_content 'Send later'
-      end
-    end
-
-    context 'disabled' do
-      before do
-        ENV['SCHEDULED_MESSAGES'] = 'false'
-        visit client_messages_path(client)
-      end
-
-      it 'shows the send later button' do
-        expect(page).not_to have_content 'Send later'
-      end
-    end
-  end
 
   describe 'signups' do
     before do
