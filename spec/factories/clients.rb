d8e5e58e5a9ef1d99e26d798b6356ebedd3fc83c
Charlie Weems <charlie@codeforamerica.org>
2017-08-30 15:40:52 -0700

    WIP initial layout of notes on clients page
    [#150031360]
    
    Signed-off-by: Lou Moore <lmoore@codeforamerica.org>

--- spec/factories/clients.rb
+++ spec/factories/clients.rb
@@ -4,6 +4,7 @@ FactoryGirl.define do
     sequence(:first_name) { Faker::Name.first_name }
     sequence(:last_name) { Faker::Name.last_name }
     sequence(:phone_number) { Faker::PhoneNumber.unique.cell_phone }
+    sequence(:notes) { Faker::Lorem.sentence }
     active true
   end
 end
--- spec/features/user_visits_messages_page_spec.rb
+++ spec/features/user_visits_messages_page_spec.rb
@@ -1,25 +1,76 @@
 require "rails_helper"
 
 feature "User clicks on client in list", :js do
-  scenario "and sees the messages page" do
-    # log in with a fake user
-    myuser = create :user
-    login_as(myuser, :scope => :user)
-    # create a new client
-    visit root_path
-    click_on "New client"
-    expect(page).to have_current_path(new_client_path)
-    myclient = build :client
-    myclient_fullname = myclient.full_name
-    add_client(myclient)
-    expect(page).to have_css '.data-table td', text: myclient_fullname
-    expect(page).to have_current_path(clients_path)
-    # click on the client
-    find('td', text: myclient_fullname).click
-    expect(page).to have_content myclient_fullname
-    expect(page).to have_content PhoneNumberParser.format_for_display(myclient.phone_number)
-    # get the id from the saved client record
-    myclient_id = Client.find_by(phone_number: PhoneNumberParser.normalize(myclient.phone_number)).id
-    expect(page).to have_current_path(client_messages_path(client_id: myclient_id))
+
+  describe "and sees the messages page" do
+    let!(:myuser) { create :user }
+    let!(:myclient) { create :client, user: myuser }
+
+    context "on the client list page" do
+      before do
+        login_as(myuser, :scope => :user)
+        visit clients_path
+      end
+
+      it "shows client name in table list" do
+        expect(page).to have_css '.data-table td', text: myclient.full_name
+      end
+
+      it "has the correct path" do
+        expect(page).to have_current_path(clients_path)
+      end
+
+      it "clicking on the client leads to client messages path" do
+        find('td', text: myclient.full_name).click
+        expect(page).to have_current_path(client_messages_path(myclient))
+      end
+    end
+
+    context "on the messages page" do
+
+      before do
+        login_as(myuser, :scope => :user)
+        visit client_messages_path(myclient)
+      end
+
+      it "shows the client's name in the header" do
+        expect(page).to have_content myclient.full_name
+      end
+
+      it "shows the client's phone number in the header" do
+        expect(page).to have_content PhoneNumberParser.format_for_display(myclient.phone_number)
+      end
+
+      it "shows the client's notes in the header" do
+        expect(page).to have_content myclient.notes
+      end
+      # get the id from the saved client record
+      it "has the correct path for the client" do
+        myclient_id = Client.find_by(phone_number: PhoneNumberParser.normalize(myclient.phone_number)).id
+        expect(page).to have_current_path(client_messages_path(client_id: myclient_id))
+      end
+    end
+
+  end
+end
+
+feature "User sees client notes on messages page", :js do
+  context "visits clients page on mobile" do
+    let!(:myuser) { create :user }
+    let!(:myclient) { create :client, user: myuser }
+
+    before do
+      resize_window_to_mobile
+    end
+
+    after do
+      resize_window_default
+    end
+
+    it "hides notes on mobile" do
+      login_as(myuser, :scope => :user)
+      visit client_messages_path(myclient)
+      expect(page).to_not have_content myclient.notes
+    end
   end
 end
