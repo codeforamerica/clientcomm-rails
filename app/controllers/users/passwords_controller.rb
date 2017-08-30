7820e13a1965213286f471c677d322b8d2a9d9b1
Charlie Weems <charlie@codeforamerica.org>
2017-08-30 11:06:01 -0700

    Make separate handler for edit / update actions on password reset
    [#150450384]
    
    Signed-off-by: Zak Auerbach <zauerbach@codeforamerica.org>

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,10 +1,18 @@
 class Users::PasswordsController < Devise::PasswordsController
-  append_before_filter :assert_reset_token_valid, :only => [:edit, :update]
+  append_before_filter :assert_reset_link_valid, :only => :edit
+  append_before_filter :assert_reset_token_valid, :only => :update
 
   protected
   # Check the password token is valid
-  def assert_reset_token_valid
+  def assert_reset_link_valid
     if !find_user_by_token(params[:reset_password_token])
+      set_flash_message(:alert, :invalid_token)
+      redirect_to new_user_password_path
+    end
+  end
+
+  def assert_reset_token_valid
+    if !find_user_by_token(params[:user][:reset_password_token])
       set_flash_message(:alert, :invalid_token)
       redirect_to new_user_password_path
     end
