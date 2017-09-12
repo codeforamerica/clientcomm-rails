3aa0a39fb951713368b3bbbc5be0f4539b47a93b
Paras Sanghavi <paras@codeforamerica.org>
2017-09-12 14:08:45 -0700

    Admins cannot disable users with active clients
    [#150758659]
    
    Signed-off-by: Charlie Weems <charlie@codeforamerica.org>

--- spec/features/admin_spec.rb
+++ spec/features/admin_spec.rb
@@ -1,9 +1,17 @@
 require 'rails_helper'
 
 feature 'Admin features' do
-  let!(:user) { create :user }
-
   scenario 'Admin disables user' do
+    step 'given there is an active user with clients' do
+      @user_1 = create :user, active: true
+      @client = create :client, user: @user_1, active: true
+      archived_client = create :client, user: @user_1, active: false
+    end
+
+    step 'given there is a second user to transfer to' do
+      @user_2 = create :user, active: true, full_name: 'Cat Stevens'
+    end
+
     step 'log in to admin panel' do
       admin = create :admin_user
       login_as(admin, scope: :admin_user)
@@ -12,16 +20,46 @@ feature 'Admin features' do
       expect(page).to have_content 'Users'
     end
 
-    step 'admin disables user' do
-      expect(find("tr##{dom_id(user)}").text).to include 'Disable'
+    step 'admin cannot disable user with active clients' do
+      expect(find("tr##{dom_id(@user_1)}").text).to include 'Disable'
 
-      within "tr##{dom_id(user)}" do
+      within "tr##{dom_id(@user_1)}" do
+        click_on 'Disable'
+      end
+
+      expect(page).to have_content "Disable #{@user_1.full_name}'s account"
+      expect(page).to have_content 'This user has active clients assigned to them.'
+    end
+
+    step 'admin transfers active clients' do
+      click_on 'Clients'
+      expect(page).to have_css('#page_title', text: 'Clients')
+
+      within "tr##{dom_id(@client)}" do
+        click_on 'Edit'
+      end
+
+      expect(page).to have_content 'Edit Client'
+      select @user_2.full_name, from: 'client_user_id'
+      click_on 'Update Client'
+    end
+
+    step 'admin navigates to user panel' do
+      click_on 'Users'
+
+      expect(page).to have_css('#page_title', text: 'Users')
+    end
+
+    step 'admin disables user' do
+      expect(find("tr##{dom_id(@user_1)}").text).to include 'Disable'
+
+      within "tr##{dom_id(@user_1)}" do
         click_on 'Disable'
       end
     end
 
     step 'admin confirms the disable action' do
-      expect(page).to have_content "Disable #{user.full_name}'s account"
+      expect(page).to have_content "Disable #{@user_1.full_name}'s account"
       click_on 'Disable account'
     end
 
@@ -30,13 +68,13 @@ feature 'Admin features' do
     end
 
     step 'admin re-enables user' do
-      expect(find("tr##{dom_id(user)}").text).to include 'Enable'
+      expect(find("tr##{dom_id(@user_1)}").text).to include 'Enable'
 
-      within "tr##{dom_id(user)}" do
+      within "tr##{dom_id(@user_1)}" do
         click_on 'Enable'
       end
 
-      expect(find("tr##{dom_id(user)}").text).to include 'Disable'
+      expect(find("tr##{dom_id(@user_1)}").text).to include 'Disable'
     end
   end
 end
