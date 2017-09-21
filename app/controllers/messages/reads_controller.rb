06e79d9c7d1e4e1a4fe3c1a5d83ac8d7823e830b
Charlie Weems <charlie@codeforamerica.org>
2017-09-21 12:32:11 -0700

    Attempt to remove StaleObjectError in tests
     * Add spec for reads_controller
    
     [#151306408]
    
    Signed-off-by: Paras Sanghavi <paras@codeforamerica.org>

--- app/controllers/messages/reads_controller.rb
+++ app/controllers/messages/reads_controller.rb
@@ -3,18 +3,16 @@ class Messages::ReadsController < ApplicationController
   skip_after_action :intercom_rails_auto_include
 
   def create
+    current_user.messages.find params[:message_id]
+
     # change the read status of the message
-    message.update!(read: message_params[:read])
+    message = Message.update(params[:message_id], read: message_params[:read])
     message.client.update!(has_unread_messages: false)
 
     head :no_content
   end
 
   private
-
-  def message
-    current_user.messages.find params[:message_id]
-  end
 
   def message_params
     params.require(:message)
---
+++ spec/requests/reads_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+describe 'reads', type: :request do
+  let(:correct_user) { create :user }
+  let(:invalid_user) { create :user }
+
+
+  context 'authenticated' do
+    before do
+      sign_in correct_user
+    end
+
+    describe 'reads#create' do
+      let(:client) { create :client, has_unread_messages: true }
+      let(:message) { create :message, client: client, read: false, user: user }
+      let(:user) { correct_user }
+
+      subject do
+        post message_read_path(message, {
+          message: {
+            read: true
+          }
+        })
+      end
+
+      it 'updates message read' do
+        subject
+
+        expect(Message.find(message.id).read).to eq true
+      end
+
+      it 'updates client has_unread_messages' do
+        subject
+
+        expect(Client.find(client.id).has_unread_messages).to eq false
+      end
+
+      context 'message does not belong to user' do
+        let(:user) { invalid_user }
+
+        it 'throws an error if message does not belong to user' do
+          expect { subject }.to raise_error(ActiveRecord::RecordNotFound)
+        end
+      end
+    end
+  end
+end
