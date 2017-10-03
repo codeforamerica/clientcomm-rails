503372696d95b6ce76fd6aba59f88e7b07f1b97c
Charlie Weems <charlie@codeforamerica.org>
2017-10-03 16:30:25 -0700

    Correctly change table names
    [#150863740]
    
    Signed-off-by: Zak Auerbach <zauerbach@codeforamerica.org>

--- db/migrate/20171003212709_rename_attachment_to_legacy_attachment.rb
+++ db/migrate/20171003212709_rename_attachment_to_legacy_attachment.rb
@@ -1,5 +1,5 @@
 class RenameAttachmentToLegacyAttachment < ActiveRecord::Migration[5.1]
   def change
-    rename_table :legacy_attachments, :legacy_attachments
+    rename_table :attachments, :legacy_attachments
   end
 end
