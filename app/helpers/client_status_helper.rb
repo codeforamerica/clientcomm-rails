1be4cdfab0e1fd819d8ec48c2fab97957f408e72
Paras Sanghavi <paras@codeforamerica.org>
2017-10-19 18:09:23 -0700

    Small style changes [#150499860]

--- app/helpers/client_status_helper.rb
+++ app/helpers/client_status_helper.rb
@@ -7,15 +7,14 @@ module ClientStatusHelper
 
     ClientStatus.all.each do |status|
       if (due_date = days_lookup.fetch(status.name, nil))
-
-        @status_hash = {}
         followup_date = Time.now - (due_date - notice_days).days
         found_clients = Client.where(["client_status_id = :id and last_contacted_at < :followup_date", {id: status.id, followup_date: followup_date}])
 
         if found_clients.count > 0
-          @status_hash['clients'] = found_clients
-          @status_hash['client_ids'] = found_clients.each { |c| c.id }
-          @output[status.name] = @status_hash
+          @output[status.name] = {
+            clients: found_clients,
+            client_ids: found_clients.each { |c| c.id }
+          }
         end
       end
     end
