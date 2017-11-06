0c9ae86ebc6224749e4d6e6682d996e5dd097140
Zak Auerbach <zauerbach@codeforamerica.org>
2017-11-06 14:25:41 -0800

    fix flakey test
    Signed-off-by: Tomas Apodaca <tomas@codeforamerica.org>

--- app/controllers/messages/reads_controller.rb
+++ app/controllers/messages/reads_controller.rb
@@ -3,10 +3,7 @@ class Messages::ReadsController < ApplicationController
   skip_after_action :intercom_rails_auto_include
 
   def create
-    current_user.messages.find params[:message_id]
-
-    # change the read status of the message
-    message = Message.update(params[:message_id], read: message_params[:read])
+    message = current_user.messages.update(params[:message_id], read: message_params[:read])
     message.client.update!(has_unread_messages: false)
 
     head :no_content
