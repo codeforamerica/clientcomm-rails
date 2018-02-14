fbc9a52b5d34937e78365728e3d17d1085eda35f
Mikela Clemmons <glassresistor@gmail.com>
2018-02-14 15:27:07 -0800

    Reports only run on current day
    [#154787747]

--- lib/tasks/reports.rake
+++ lib/tasks/reports.rake
@@ -1,13 +1,18 @@
 namespace :reports do
   task :generate_and_send_reports => :environment do
     end_date = Time.now
-    Department.all.each do |department|
-      recipients = department.reports.pluck(:email)
-      metrics = department.message_metrics(end_date)
-      recipients.each do |recipient|
-        NotificationMailer.report_usage(recipient, metrics, end_date.to_s)
-                          .deliver_later
+    if end_date.wday.to_s == ENV['REPORT_DAY']
+      Department.all.each do |department|
+        recipients = department.reports.pluck(:email)
+        metrics = department.message_metrics(end_date)
+        recipients.each do |recipient|
+          NotificationMailer.report_usage(recipient, metrics, end_date.to_s)
+                            .deliver_later
+        end
       end
+      Rails.logger.info 'Report task ran and sent reports'
+    else
+      Rails.logger.info 'Report task ran but sent no reports'
     end
   end
 end
--- spec/tasks/reports_spec.rb
+++ spec/tasks/reports_spec.rb
@@ -2,24 +2,45 @@ require 'rails_helper'
 require 'rake'
 
 describe 'reports rake tasks' do
-  describe 'reports:generate_and_send_reports' do
+  describe 'reports:generate_and_send_reports', type: :request do
     let(:department) { create :department }
     let(:report_count) { 5 }
     let!(:report_list) { create_list :report, report_count, department: department }
-    let(:now) { Time.now.change(usec: 0) }
+    let(:report_date) { Time.now.change(usec: 0, hour: 5) }
+    let(:day_of_week) { report_date }
+
     before do
-      load File.expand_path('../../../lib/tasks/reports.rake', __FILE__)
+      Rake.application.rake_require 'tasks/reports'
       Rake::Task.define_task(:environment)
+      @weekday = ENV['REPORT_DAY']
+      ENV['REPORT_DAY'] = day_of_week.wday.to_s
     end
-    it 'calls NotificationMailer.report_usage' do
-      metrics = department.message_metrics(now)
+
+    after do
+      ENV['REPORT_DAY'] = @weekday
+    end
+
+    it 'calls NotificationMailer.report_usage part' do
+      metrics = department.message_metrics(report_date)
       report_list.each do |report|
         expect(NotificationMailer).to receive(:report_usage)
-          .with(report.email, metrics, now)
+          .with(report.email, metrics, report_date.to_s)
           .and_return(double('NotificationMailer', deliver_later: true))
       end
-      travel_to now do
-        Rake::Task['reports:generate_and_send_reports'].invoke
+      travel_to report_date do
+        Rake::Task['reports:generate_and_send_reports'].reenable
+        Rake.application.invoke_task 'reports:generate_and_send_reports'
+      end
+    end
+    context 'on incorrect day' do
+      let(:day_of_week) { report_date - 1.day }
+      it 'does not call NotificationMailer.report_usage part two' do
+        metrics = department.message_metrics(report_date)
+        expect(NotificationMailer).to_not receive(:report_usage)
+        travel_to report_date do
+          Rake::Task['reports:generate_and_send_reports'].reenable
+          Rake.application.invoke_task 'reports:generate_and_send_reports'
+        end
       end
     end
   end
