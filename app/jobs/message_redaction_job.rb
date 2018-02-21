45c24f770570faf34414fd778a74ab6921e82eba
Zak Auerbach <zauerbach@codeforamerica.org>
2018-02-21 14:14:30 -0800

    Retry activejob for redaction on twilio 404
    [#152495655]

--- app/jobs/message_redaction_job.rb
+++ app/jobs/message_redaction_job.rb
@@ -1,7 +1,14 @@
+class TwilioNotFound < StandardError; end
+
 class MessageRedactionJob < ApplicationJob
   queue_as :default
 
+  retry_on TwilioNotFound
+
   def perform(message:)
     SMSService.instance.redact_message(message: message)
+  rescue Twilio::REST::RestError => e
+    raise TwilioNotFound if e.code == 20404
+    raise e
   end
 end
