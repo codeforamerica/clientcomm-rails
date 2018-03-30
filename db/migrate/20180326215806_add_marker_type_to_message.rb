7a67d3b270be4b1f665f62be01167934a7dc3e4e
Tomas Apodaca <tomas@codeforamerica.org>
2018-03-30 12:57:35 -0700

    consolidate and test migration
    [#155714119]

--- db/migrate/20180326215806_add_marker_type_to_message.rb
+++
@@ -1,6 +0,0 @@
-class AddMarkerTypeToMessage < ActiveRecord::Migration[5.1]
-  def change
-    add_column :messages, :marker_type, :int, null: true, default: nil
-    add_index :messages, :marker_type
-  end
-end
---
+++ spec/migrations/move_message_transfer_marker_to_marker_type_spec.rb
@@ -0,0 +1,100 @@
+require 'rails_helper.rb'
+require_relative '../../db/migrate/20180326220423_move_message_transfer_marker_to_marker_type'
+
+describe MoveMessageTransferMarkerToMarkerType do
+  let(:user) { create :user }
+  let(:client) { create :client, users: [user] }
+
+  after do
+    ActiveRecord::Migrator.migrate Rails.root.join('db', 'migrate'), @current_migration
+    Message.reset_column_information
+  end
+
+  describe '#up' do
+    before do
+      @current_migration = ActiveRecord::Migrator.current_version
+      ActiveRecord::Migrator.migrate Rails.root.join('db', 'migrate'), 20180314000756
+      Message.reset_column_information
+    end
+
+    subject do
+      ActiveRecord::Migrator.migrate Rails.root.join('db', 'migrate'), 20180326220423
+      Message.reset_column_information
+    end
+
+    it 'messages have proper values on marker_type' do
+      rr = ReportingRelationship.find_by(user: user, client: client)
+
+      message_marker = Message.create!(
+        reporting_relationship_id: rr.id,
+        original_reporting_relationship_id: rr.id,
+        transfer_marker: true,
+        read: true,
+        send_at: Time.now,
+        inbound: true,
+        number_to: rr.user.department.phone_number,
+        number_from: rr.client.phone_number
+      )
+
+      message_message = Message.create!(
+        reporting_relationship_id: rr.id,
+        original_reporting_relationship_id: rr.id,
+        transfer_marker: false,
+        read: true,
+        send_at: Time.now,
+        inbound: true,
+        number_to: rr.user.department.phone_number,
+        number_from: rr.client.phone_number
+      )
+
+      subject
+
+      expect(message_message.reload.marker_type).to eq(nil)
+      expect(message_marker.reload.marker_type).to eq(Message::MARKER_TRANSFER)
+    end
+  end
+
+  describe '#down' do
+    before do
+      @current_migration = ActiveRecord::Migrator.current_version
+      ActiveRecord::Migrator.migrate Rails.root.join('db', 'migrate'), 20180326220423
+      Message.reset_column_information
+    end
+
+    subject do
+      ActiveRecord::Migrator.migrate Rails.root.join('db', 'migrate'), 20180314000756
+      Message.reset_column_information
+    end
+
+    it 'messages have proper values on transfer_marker' do
+      rr = ReportingRelationship.find_by(user: user, client: client)
+
+      message_marker = Message.create!(
+        reporting_relationship_id: rr.id,
+        original_reporting_relationship_id: rr.id,
+        marker_type: Message::MARKER_TRANSFER,
+        read: true,
+        send_at: Time.now,
+        inbound: true,
+        number_to: rr.user.department.phone_number,
+        number_from: rr.client.phone_number
+      )
+
+      message_message = Message.create!(
+        reporting_relationship_id: rr.id,
+        original_reporting_relationship_id: rr.id,
+        marker_type: nil,
+        read: true,
+        send_at: Time.now,
+        inbound: true,
+        number_to: rr.user.department.phone_number,
+        number_from: rr.client.phone_number
+      )
+
+      subject
+
+      expect(message_message.reload.transfer_marker).to eq(false)
+      expect(message_marker.reload.transfer_marker).to eq(true)
+    end
+  end
+end
