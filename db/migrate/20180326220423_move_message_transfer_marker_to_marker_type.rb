dceb0eab2b7bcec28da8b5b3fb4165c2d7a304ba
Zak Auerbach <zak@zaksoup.com>
2018-04-04 16:05:56 -0700

    Do not validate messages on save
    Log batches of 1k messages as they are migrated

--- db/migrate/20180326220423_move_message_transfer_marker_to_marker_type.rb
+++ db/migrate/20180326220423_move_message_transfer_marker_to_marker_type.rb
@@ -9,21 +9,31 @@ class MoveMessageTransferMarkerToMarkerType < ActiveRecord::Migration[5.1]
 
     reversible do |dir|
       dir.up do
+        msg_count = Message.all.count
+        Rails.logger.info "Migrating #{msg_count} messages"
         Message.reset_column_information
-        Message.find_each do |msg|
-          if msg.transfer_marker
-            msg.marker_type = Message::MARKER_TRANSFER
-            msg.save!
+        Message.find_in_batches.with_index do |batch, i|
+          Rails.logger.info "Migrating batch #{i} of #{msg_count / 1000}"
+          batch.each do |msg|
+            if msg.transfer_marker
+              msg.marker_type = Message::MARKER_TRANSFER
+              msg.save validate: false
+            end
           end
         end
       end
 
       dir.down do
+        msg_count = Message.all.count
+        Rails.logger.info "Migrating #{msg_count} messages"
         Message.reset_column_information
-        Message.find_each do |msg|
-          if msg.marker_type == Message::MARKER_TRANSFER
-            msg.transfer_marker = true
-            msg.save!
+        Message.find_in_batches.with_index do |batch, i|
+          Rails.logger.info "Migrating batch #{i} of #{msg_count / 1000}"
+          batch.each do |msg|
+            if msg.marker_type == Message::MARKER_TRANSFER
+              msg.transfer_marker = true
+              msg.save validate: false
+            end
           end
         end
       end
