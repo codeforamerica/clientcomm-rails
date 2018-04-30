cef92c8d71b9c225eae2df16856f25d3a56f43e9
Zak Auerbach <zauerbach@codeforamerica.org>
2018-04-30 13:50:15 -0700

    Rubocop fix: Style/ClassAndModuleChildren

--- app/controllers/messages/reads_controller.rb
+++ app/controllers/messages/reads_controller.rb
@@ -1,19 +1,21 @@
-class Messages::ReadsController < ApplicationController
-  before_action :authenticate_user!
-  skip_after_action :intercom_rails_auto_include
+module Messages
+  class ReadsController < ApplicationController
+    before_action :authenticate_user!
+    skip_after_action :intercom_rails_auto_include
 
-  def create
-    message = current_user.messages.update(params[:message_id], read: message_params[:read])
+    def create
+      message = current_user.messages.update(params[:message_id], read: message_params[:read])
 
-    message.reporting_relationship.update!(has_unread_messages: false)
+      message.reporting_relationship.update!(has_unread_messages: false)
 
-    head :no_content
-  end
+      head :no_content
+    end
 
-  private
+    private
 
-  def message_params
-    params.require(:message)
-          .permit(:read)
+    def message_params
+      params.require(:message)
+            .permit(:read)
+    end
   end
 end
