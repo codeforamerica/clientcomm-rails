cc6923199b1c2136e6c431090b4996201508a69c
Zak Auerbach <zauerbach@codeforamerica.org>
2018-04-30 10:42:25 -0700

    Rubocop fix: Rails/ReversibleMigration

--- db/migrate/20170718001857_remove_birth_date_from_clients.rb
+++ db/migrate/20170718001857_remove_birth_date_from_clients.rb
@@ -1,5 +1,5 @@
 class RemoveBirthDateFromClients < ActiveRecord::Migration[5.0]
   def change
-    remove_column :clients, :birth_date
+    remove_column :clients, :birth_date, :datetime
   end
 end
