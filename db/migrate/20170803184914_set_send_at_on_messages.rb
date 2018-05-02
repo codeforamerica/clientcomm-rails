2b2d4e81d620cbd4282632d0717f87a3c1374e4c
Zak Auerbach <zauerbach@codeforamerica.org>
2018-05-01 17:01:16 -0700

    Rubocop fix: postfix if

--- db/migrate/20170803184914_set_send_at_on_messages.rb
+++ db/migrate/20170803184914_set_send_at_on_messages.rb
@@ -1,9 +1,7 @@
 class SetSendAtOnMessages < ActiveRecord::Migration[5.0]
   def change
     Message.all.each do |message|
-      if message.send_at.nil?
-        message.update(send_at: message.created_at)
-      end
+      message.update(send_at: message.created_at) if message.send_at.nil?
     end
 
     change_column :messages, :send_at, :datetime, null: false
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,9 +25,7 @@ RSpec.configure do |config|
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
-  if config.files_to_run.one?
-    config.default_formatter = 'doc'
-  end
+  config.default_formatter = 'doc' if config.files_to_run.one?
 
   config.include Paperclip::Shoulda::Matchers
 
