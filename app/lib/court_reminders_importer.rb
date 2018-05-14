9ad9fe5a7fc646a92b0468719c2c576b3e2502f7
Mikela Clemmons <glassresistor@gmail.com>
2018-05-14 11:19:54 -0700

    Update logging, add locations
    [#157564753]
    
    Co-authored-by: Zak Auerbach <zak@codeforamerica.org>

--- app/lib/court_reminders_importer.rb
+++ app/lib/court_reminders_importer.rb
@@ -4,42 +4,45 @@ module CourtRemindersImporter
 
     Message.transaction do
       Message.scheduled.auto_court_reminders.destroy_all unless options[:dry_run]
-      court_dates.each do |court_date|
-        matching_rrs = ReportingRelationship.where(notes: court_date['ofndr_num'], active: true)
-        Rails.logger.info { "[importing court reminders] Found #{matching_rrs.count} RRs with IDs #{matching_rrs.pluck(:id)} with the same ctrack" }
+      Rails.logger.tagged('court reminders') do
+        court_dates.each do |court_date|
+          Rails.logger.info { "Creating reminder for ctrack #{court_date['ofndr_num']}" }
+          matching_rrs = ReportingRelationship.where(notes: court_date['ofndr_num'], active: true)
+          Rails.logger.info { "Found #{matching_rrs.count} RRs with IDs #{matching_rrs.pluck(:id)} with the same ctrack" }
 
-        rr = matching_rrs.left_joins(:messages).order('messages.send_at DESC NULLS LAST').first
-        next if rr.nil?
+          rr = matching_rrs.left_joins(:messages).order('messages.send_at DESC NULLS LAST').first
+          next if rr.nil?
 
-        Rails.logger.tagged("Reminder for Client: #{rr.client.id}") do
-          court_date_at = Time.strptime("#{court_date['crt_dt']} #{court_date['crt_tm']} #{time_zone_offset}", '%m/%d/%Y %H:%M %z')
-          next if court_date_at < Time.zone.now
+          Rails.logger.tagged("client: #{rr.client.id}") do
+            court_date_at = Time.strptime("#{court_date['crt_dt']} #{court_date['crt_tm']} #{time_zone_offset}", '%m/%d/%Y %H:%M %z')
+            next if court_date_at < Time.zone.now
 
-          body = I18n.t(
-            'message.auto_court_reminder',
-            location: court_locations[court_date['(expression)']],
-            date: court_date_at.strftime('%-m/%-d/%Y'),
-            time: court_date_at.strftime('%-l:%M%P'),
-            room: court_date['crt_rm']
-          )
+            body = I18n.t(
+              'message.auto_court_reminder',
+              location: court_locations[court_date['(expression)']],
+              date: court_date_at.strftime('%-m/%-d/%Y'),
+              time: court_date_at.strftime('%-l:%M%P'),
+              room: court_date['crt_rm']
+            )
+            send_at = court_date_at.utc - 1.day
 
-          Rails.logger.info { "Body: \"#{body}\"" }
+            Rails.logger.info { "Body: \"#{body}\"" }
+            Rails.logger.info { "Send At: \"#{send_at}\"" }
 
-          send_at = court_date_at.utc - 1.day
+            message = Message.new(
+              body: body,
+              reporting_relationship: rr,
+              number_from: rr.user.department.phone_number,
+              number_to: rr.client.phone_number,
+              send_at: send_at,
+              read: true,
+              marker_type: Message::AUTO_COURT_REMINDER
+            )
 
-          message = Message.new(
-            body: body,
-            reporting_relationship: rr,
-            number_from: rr.user.department.phone_number,
-            number_to: rr.client.phone_number,
-            send_at: send_at,
-            read: true,
-            marker_type: Message::AUTO_COURT_REMINDER
-          )
-
-          unless options[:dry_run]
-            message.save!
-            message.send_message
+            unless options[:dry_run]
+              message.save!
+              message.send_message
+            end
           end
         end
       end
