b9b0b0e8f41634e826128ced2708af524071e2a2
Tomas Apodaca <tomas@codeforamerica.org>
2018-05-16 16:57:56 -0700

    continued test driving of message parsing
    [#156627235]

--- spec/lib/node_messages_importer_spec.rb
+++ spec/lib/node_messages_importer_spec.rb
@@ -4,12 +4,27 @@ describe NodeMessagesImporter do
   let(:cm_id) { '8' }
   let(:comm_id) { '9' }
   let(:message_body) { 'This is my anonymous message body' }
+  let(:message_created) { '2016-03-23 12:59:29.35076-07' }
+  let(:twilio_sid) { 'SM99999999999999999999999999999999' }
+  let(:twilio_status) { 'received' }
+  let(:read) { 't' }
+  let(:read_boolean) { true }
+  let(:inbound) { 't' }
+  let(:inbound_boolean) { true }
+  let(:client_number) { '14155554321' }
+  let(:client_number_normalized) { "+#{client_number}" }
   let(:message_segments) do
     [
       {
+        'cm' => cm_id,
         'comm' => comm_id,
         'content' => message_body,
-        'cm' => cm_id
+        'created' => message_created,
+        'inbound' => inbound,
+        'read' => read,
+        'tw_sid' => twilio_sid,
+        'tw_status' => twilio_status,
+        'value' => client_number
       }
     ]
   end
@@ -20,8 +35,19 @@ describe NodeMessagesImporter do
 
   subject { described_class.import_message(message_segments) }
 
-  it 'turns the message set into a message' do
-    subject
-    expect(rr.messages.pluck(:body)).to contain_exactly(message_body)
+  context 'parsing an incoming message' do
+    it 'creates a message that matches the passed data' do
+      subject
+      expect(rr.user).to eq(user)
+      expect(rr.client).to eq(client)
+      message = rr.messages.find_by(twilio_sid: twilio_sid)
+      expect(message).to_not be_nil
+      expect(message.body).to eq(message_body)
+      expect(message.send_at).to eq(Time.parse(message_created).utc)
+      expect(message.inbound).to eq(inbound_boolean)
+      expect(message.read).to eq(read_boolean)
+      expect(message.twilio_status).to eq(twilio_status)
+      expect(message.number_from).to eq(client_number_normalized)
+    end
   end
 end
