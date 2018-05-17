a6ec72a5fc2d7583e334019a956f1080834fa41b
Zak Auerbach <zauerbach@codeforamerica.org>
2018-05-17 15:19:41 -0700

    test drive outgoing messages
    start test for multi-segment messages
    
    [#156627235]
    
    Co-authored-by: Tomas Apodaca <tomas@codeforamerica.org>

--- app/lib/node_messages_importer.rb
+++ app/lib/node_messages_importer.rb
@@ -4,19 +4,20 @@ module NodeMessagesImporter
     body = node_message['content']
     user = User.find_by(node_id: node_message['cm'])
     client = Client.find_by(node_comm_id: node_message['comm'])
-    send_at = Time.parse(node_message['created']).utc
     rr = ReportingRelationship.find_by(user: user, client: client)
 
     return if rr.nil?
 
+    normalized_phone_number = "+#{node_message['value']}"
+
     message = Message.new(
       body: body,
-      inbound: ActiveModel::Type::Boolean.new.cast(node_message['inbound']),
-      number_from: "+#{node_message['value']}",
-      number_to: rr.department.phone_number,
-      read: ActiveModel::Type::Boolean.new.cast(node_message['read']),
+      inbound: node_message['inbound'],
+      number_from: node_message['inbound'] == 't' ? normalized_phone_number : rr.department.phone_number,
+      number_to: node_message['inbound'] == 't' ? rr.department.phone_number : normalized_phone_number,
+      read: node_message['read'],
       reporting_relationship: rr,
-      send_at: send_at,
+      send_at: node_message['created'],
       twilio_sid: node_message['tw_sid'],
       twilio_status: node_message['tw_status']
     )
