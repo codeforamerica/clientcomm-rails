46e95ec561724af13f9d3ec5dd6a15e0c856fca0
Tomas Apodaca <tomas@codeforamerica.org>
2018-05-17 20:42:18 -0700

    test drive multi-segment messages
    and stub out future tests
    
    [#156627235]

--- app/lib/node_messages_importer.rb
+++ app/lib/node_messages_importer.rb
@@ -1,25 +1,30 @@
 module NodeMessagesImporter
   def self.import_message(message_segments)
-    node_message = message_segments.first
-    body = node_message['content']
-    user = User.find_by(node_id: node_message['cm'])
-    client = Client.find_by(node_comm_id: node_message['comm'])
+    content_parts = []
+    message_segments.each do |segment|
+      content_parts << segment['content']
+    end
+    body = content_parts.join
+    first_segment = message_segments.first
+    user = User.find_by(node_id: first_segment['cm'])
+    client = Client.find_by(node_comm_id: first_segment['comm'])
     rr = ReportingRelationship.find_by(user: user, client: client)
 
     return if rr.nil?
 
-    normalized_phone_number = "+#{node_message['value']}"
+    normalized_phone_number = "+#{first_segment['value']}"
+    inbound = first_segment['inbound'] == 't'
 
     message = Message.new(
       body: body,
-      inbound: node_message['inbound'],
-      number_from: node_message['inbound'] == 't' ? normalized_phone_number : rr.department.phone_number,
-      number_to: node_message['inbound'] == 't' ? rr.department.phone_number : normalized_phone_number,
-      read: node_message['read'],
+      inbound: first_segment['inbound'],
+      number_from: inbound ? normalized_phone_number : rr.department.phone_number,
+      number_to: inbound ? rr.department.phone_number : normalized_phone_number,
+      read: inbound ? first_segment['read'] : true,
       reporting_relationship: rr,
-      send_at: node_message['created'],
-      twilio_sid: node_message['tw_sid'],
-      twilio_status: node_message['tw_status']
+      send_at: first_segment['created'],
+      twilio_sid: first_segment['tw_sid'],
+      twilio_status: first_segment['tw_status']
     )
 
     message.save!
--- spec/lib/node_messages_importer_spec.rb
+++ spec/lib/node_messages_importer_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe NodeMessagesImporter do
+  let(:convo_id) { '111' }
   let(:cm_id) { '8' }
   let(:comm_id) { '9' }
   let(:message_body) { 'This is my anonymous message body' }
@@ -8,14 +9,15 @@ describe NodeMessagesImporter do
   let(:twilio_sid) { 'SM99999999999999999999999999999999' }
   let(:twilio_status) { 'received' }
   let(:read) { 't' }
-  let(:read_boolean) { true }
+  let(:read_boolean) { ActiveModel::Type::Boolean.new.cast(read) }
   let(:inbound) { 't' }
-  let(:inbound_boolean) { true }
+  let(:inbound_boolean) { ActiveModel::Type::Boolean.new.cast(inbound) }
   let(:client_number) { '14155554321' }
   let(:client_number_normalized) { "+#{client_number}" }
   let(:message_segments) do
     [
       {
+        'convid' => convo_id,
         'cm' => cm_id,
         'comm' => comm_id,
         'content' => message_body,
@@ -36,24 +38,29 @@ describe NodeMessagesImporter do
   subject { described_class.import_message(message_segments) }
 
   context 'parsing an incoming message' do
-    it 'creates a message that matches the passed data' do
+    it 'creates an incoming message object' do
       subject
       message = rr.messages.find_by(twilio_sid: twilio_sid)
       expect(message).to_not be_nil
-      expect(message.body).to eq(message_body)
-      expect(message.send_at).to eq(Time.parse(message_created).utc)
-      expect(message.inbound).to eq(inbound_boolean)
-      expect(message.read).to eq(read_boolean)
-      expect(message.twilio_status).to eq(twilio_status)
-      expect(message.number_from).to eq(client_number_normalized)
+      expect(message.body).to eq message_body
+      expect(message.send_at).to eq Time.parse(message_created).utc
+      expect(message.inbound).to eq inbound_boolean
+      expect(message.read).to eq read_boolean
+      expect(message.twilio_status).to eq twilio_status
+      expect(message.number_from).to eq client_number_normalized
+    end
+
+    context 'the message has an attached recording' do
     end
 
     context 'the message has multiple segments' do
-      let(:message_body_segment_1) { 'This is segment 1 of my anonymous message body' }
-      let(:message_body_segment_2) { 'This is segment 2 of my anonymous message body' }
+      let(:message_body_segment_1) { 'This is segment 1 of my anonymous message body and ' }
+      let(:message_body_segment_2) { 'this text is contained in segment 2' }
+      let(:message_body_full) { [message_body_segment_1, message_body_segment_2].join }
       let(:message_segments) do
         [
           {
+            'convid' => convo_id,
             'cm' => cm_id,
             'comm' => comm_id,
             'content' => message_body_segment_1,
@@ -65,6 +72,7 @@ describe NodeMessagesImporter do
             'value' => client_number
           },
           {
+            'convid' => convo_id,
             'cm' => cm_id,
             'comm' => comm_id,
             'content' => message_body_segment_2,
@@ -77,18 +85,40 @@ describe NodeMessagesImporter do
           }
         ]
       end
+
+      it 'creates a single message object' do
+        subject
+        messages = rr.messages.where(twilio_sid: twilio_sid)
+        expect(messages.count).to eq 1
+        message = messages.first
+        expect(message.body).to eq message_body_full
+        expect(message.send_at).to eq Time.parse(message_created).utc
+        expect(message.inbound).to eq inbound_boolean
+        expect(message.read).to eq read_boolean
+        expect(message.twilio_status).to eq twilio_status
+        expect(message.number_from).to eq client_number_normalized
+      end
+
+      context 'there are duplicate message segments' do
+        context 'only one of the duplicates can be matched to an active relationship' do
+        end
+        context 'multiple duplicates belong to the same active relationship' do
+        end
+      end
     end
   end
 
   context 'parsing an outgoing message' do
     let(:inbound) { 'f' }
-    let(:inbound_boolean) { false }
+    let(:read) { 'f' }
 
-    it 'creates a message that matches the passed data' do
+    it 'creates an outgoing message object' do
       subject
       message = rr.messages.find_by(twilio_sid: twilio_sid)
-      expect(message.number_from).to eq(user.department.phone_number)
-      expect(message.number_to).to eq(client_number_normalized)
+      expect(message.inbound).to eq inbound_boolean
+      expect(message.number_from).to eq user.department.phone_number
+      expect(message.number_to).to eq client_number_normalized
+      expect(message.read).to eq true
     end
   end
 end
