d02954d8ab15d7a97b60286c939908fbca527096
Mikela Clemmons <glassresistor@gmail.com>
2018-06-07 11:47:04 -0700

    Fixed breaking migrations due to CSV inflector

--- db/migrate/20180518203229_create_court_date_csv.rb
+++ db/migrate/20180518203229_create_court_date_csv.rb
@@ -1,4 +1,4 @@
-class CreateCourtDateCsv < ActiveRecord::Migration[5.1]
+class CreateCourtDateCSV < ActiveRecord::Migration[5.1]
   def change
     create_table :court_date_csvs do |t|
       t.attachment :file
