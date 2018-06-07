2e57d15320a9e458865551c2d5d8be188dd449d4
Mikela Clemmons <glassresistor@gmail.com>
2018-06-07 11:28:14 -0700

    rubocop

--- spec/factories/court_date_csvs.rb
+++ spec/factories/court_date_csvs.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :court_date_csv do
-    file { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec', 'fixtures', 'court_dates.csv'), 'text/csv') }
+    file { Rack::Test::UploadedFile.new(Rails.root.join('spec', 'fixtures', 'court_dates.csv'), 'text/csv') }
     admin_user { create :admin_user }
   end
 end
