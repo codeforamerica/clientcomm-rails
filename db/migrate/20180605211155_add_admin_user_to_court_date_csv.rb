cade8e71b6f423de89088bc32d33418eeebcd3c8
Mikela Clemmons <glassresistor@gmail.com>
2018-06-07 11:47:04 -0700

    Fixed breaking migrations due to CSV inflector

--- db/migrate/20180605211155_add_admin_user_to_court_date_csv.rb
+++ db/migrate/20180605211155_add_admin_user_to_court_date_csv.rb
@@ -1,4 +1,4 @@
-class AddAdminUserToCourtDateCsv < ActiveRecord::Migration[5.1]
+class AddAdminUserToCourtDateCSV < ActiveRecord::Migration[5.1]
   def change
     add_reference :court_date_csvs, :admin_user, foreign_key: { to_table: :admin_users }, null: false, default: 1
   end
--- db/migrate/20180606182912_add_court_date_csv_to_message.rb
+++ db/migrate/20180606182912_add_court_date_csv_to_message.rb
@@ -1,4 +1,4 @@
-class AddCourtDateCsVtoMessage < ActiveRecord::Migration[5.1]
+class AddCourtDateCSVToMessage < ActiveRecord::Migration[5.1]
   def change
     add_reference :messages, :court_date_csv, foreign_key: { to_table: :court_date_csvs }
   end
