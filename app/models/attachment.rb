fd212fb8bab288dc68cd7181c64d2fe60de6c4e9
Mikela Clemmons <glassresistor@gmail.com>
2018-06-22 16:03:24 -0700

    removed unnessary remote url setter

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -5,12 +5,6 @@ class Attachment < ApplicationRecord
   has_attached_file :media
   validates_attachment_content_type :media, content_type: /video\/.*|audio\/.*|image\/.*|text\/.*|application\/pdf.*/
 
-  # Save attachment content with Paperclip
-  def media_remote_url=(url_value)
-    self.media = URI.parse(url_value)
-    @media_remote_url = url_value
-  end
-
   def image?
     media_content_type.match?(/image\/.*/) ? true : false
   end
