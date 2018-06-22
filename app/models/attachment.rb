0e3019b878ae7f57b1bb01290e8a8a7c4907384b
Mikela Clemmons <glassresistor@gmail.com>
2018-06-22 16:04:56 -0700

    removed unnessary remote url setter

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,7 +1,6 @@
 class Attachment < ApplicationRecord
   belongs_to :message
 
-  attr_reader :media_remote_url
   has_attached_file :media
   validates_attachment_content_type :media, content_type: /video\/.*|audio\/.*|image\/.*|text\/.*|application\/pdf.*/
 
