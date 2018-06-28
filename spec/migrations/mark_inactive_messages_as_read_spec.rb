4b3f3d397aadf7fe5a020767f9c814e8f7571c53
Tomas Apodaca <tomas@codeforamerica.org>
2018-06-28 10:57:13 -0700

    added a migration test
    [#156354138]

--- spec/migrations/mark_inactive_messages_as_read_spec.rb
+++ spec/migrations/mark_inactive_messages_as_read_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper.rb'
 require_relative '../../db/migrate/20180627210514_mark_inactive_messages_as_read.rb'
 
 describe MarkInactiveMessagesAsRead do
-  let(:rr) { create :reporting_relationship, active: false }
+  let!(:rr) { create :reporting_relationship, active: false }
 
   after do
     ActiveRecord::Migrator.migrate Rails.root.join('db', 'migrate'), @current_migration
@@ -21,7 +21,7 @@ describe MarkInactiveMessagesAsRead do
       Message.reset_column_information
     end
 
-    it 'marks unread messages as read' do
+    it 'marks unread message as read' do
       Message.create!(
         reporting_relationship_id: rr.id,
         original_reporting_relationship_id: rr.id,
@@ -32,13 +32,17 @@ describe MarkInactiveMessagesAsRead do
         number_from: rr.client.phone_number,
         number_to: rr.user.department.phone_number
       )
+
       subject
+
       expect(rr.messages.where(read: false)).to be_empty
     end
+
     context 'rr is active' do
-      let(:rr) { create :reporting_relationship, active: true }
-      it 'marks unread messages as read' do
-        text_message = Message.create!(
+      let!(:rr) { create :reporting_relationship, active: true }
+
+      it 'does not mark unread message as read' do
+        message = Message.create!(
           reporting_relationship_id: rr.id,
           original_reporting_relationship_id: rr.id,
           read: false,
@@ -48,8 +52,44 @@ describe MarkInactiveMessagesAsRead do
           number_from: rr.client.phone_number,
           number_to: rr.user.department.phone_number
         )
+
         subject
-        expect(rr.messages.where(read: false)).to contain_exactly(text_message)
+
+        expect(rr.messages.where(read: false)).to contain_exactly(message)
+      end
+    end
+
+    context 'the client has a second, active rr' do
+      let!(:user2) { create :user, department: rr.user.department }
+      let!(:rr2) { create :reporting_relationship, client: rr.client, user: user2, active: true }
+
+      it 'marks only the unread message on the inactive rr as read' do
+        Message.create!(
+          reporting_relationship_id: rr.id,
+          original_reporting_relationship_id: rr.id,
+          read: false,
+          inbound: true,
+          send_at: Time.zone.now,
+          type: 'TextMessage',
+          number_from: rr.client.phone_number,
+          number_to: rr.user.department.phone_number
+        )
+
+        message2 = Message.create!(
+          reporting_relationship_id: rr2.id,
+          original_reporting_relationship_id: rr2.id,
+          read: false,
+          inbound: true,
+          send_at: Time.zone.now,
+          type: 'TextMessage',
+          number_from: rr2.client.phone_number,
+          number_to: rr2.user.department.phone_number
+        )
+
+        subject
+
+        expect(rr.messages.where(read: false)).to be_empty
+        expect(rr2.messages.where(read: false)).to contain_exactly(message2)
       end
     end
   end
