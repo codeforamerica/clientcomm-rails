a2503d6e2f6ac5f79ac4c95062079f3997a41314
Mikela Clemmons <glassresistor@gmail.com>
2018-08-09 16:23:32 -0700

    ScheduledMessageJob should log its own start and finish
    * Log every job and its id
    * Add metrics for run time
    
    [#158959060]

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,36 +1,60 @@
 class CloudWatchPlugin < Delayed::Plugin
   callbacks do |lifecycle|
     lifecycle.around(:invoke_job) do |job, *args, &block|
-      begin
-        block.call(job, *args)
-      # rubocop:disable Lint/RescueException
-      rescue Exception => error
-        CLOUD_WATCH.put_metric_data(
-          namespace: ENV['DEPLOYMENT'],
-          metric_data: [
-            {
-              metric_name: 'JobRaisedError',
-              timestamp: Time.zone.now,
-              value: 1.0,
-              unit: 'None',
-              storage_resolution: 1
-            }
-          ]
-        )
-        raise error
-      else
-        CLOUD_WATCH.put_metric_data(
-          namespace: ENV['DEPLOYMENT'],
-          metric_data: [
-            {
-              metric_name: 'JobSucceeded',
-              timestamp: Time.zone.now,
-              value: 1.0,
-              unit: 'None',
-              storage_resolution: 1
-            }
-          ]
-        )
+      start_time = Time.zone.now
+      handler = YAML.parse(job.handler).to_ruby
+      job_class = handler.job_data['job_class']
+      job_id = handler.job_data['job_id']
+      Rails.logger.tagged("Job class: #{job_class} id: #{job_id}") do
+        Rails.logger.info { 'BEGINS' }
+        begin
+          block.call(job, *args)
+          # rubocop:disable Lint/RescueException
+        rescue Exception => error
+          Rails.logger.warn { 'ERRORED' }
+          CLOUD_WATCH.put_metric_data(
+            namespace: ENV['DEPLOYMENT'],
+            metric_data: [
+              {
+                metric_name: 'JobRaisedError',
+                timestamp: Time.zone.now,
+                value: 1.0,
+                unit: 'None',
+                storage_resolution: 1
+              }
+            ]
+          )
+          raise error
+        else
+          Rails.logger.warn { 'SUCCEEDED' }
+          CLOUD_WATCH.put_metric_data(
+            namespace: ENV['DEPLOYMENT'],
+            metric_data: [
+              {
+                metric_name: 'JobSucceeded',
+                timestamp: Time.zone.now,
+                value: 1.0,
+                unit: 'None',
+                storage_resolution: 1
+              }
+            ]
+          )
+        ensure
+          time_delta = (Time.zone.now - start_time) * 1000
+          Rails.logger.warn { "Job Ran in #{time_delta}ms" }
+          CLOUD_WATCH.put_metric_data(
+            namespace: ENV['DEPLOYMENT'],
+            metric_data: [
+              {
+                metric_name: 'JobRunTime',
+                timestamp: Time.zone.now,
+                value: 1.0,
+                unit: 'Milliseconds',
+                storage_resolution: 1
+              }
+            ]
+          )
+        end
       end
     end
   end
