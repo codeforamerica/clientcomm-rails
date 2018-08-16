a6976ab8e69c5a5ab700e47f00f6d4b1819df873
Mikela Clemmons <glassresistor@gmail.com>
2018-08-16 16:57:31 -0700

    running update twice

--- db/migrate/20180816222016_cleanup_message_history.rb
+++ db/migrate/20180816222016_cleanup_message_history.rb
@@ -2,6 +2,7 @@ class CleanupMessageHistory < ActiveRecord::Migration[5.1]
   def up
     past_messages = TextMessage.where(sent: false).where(inbound: false).where('send_at < ?', Time.zone.now - 1.hour)
     past_messages.where.not(twilio_sid: nil).update(sent: true)
-    past_messages.where(twilio_sid: nil).update(sent: true, twilio_status: 'undelivered')
+    past_messages.where(twilio_sid: nil).update(twilio_status: 'undelivered')
+    past_messages.where(twilio_sid: nil).update(sent: true)
   end
 end
