7b3a0608536c74020136447de6c02f5803404d33
Mikela Clemmons <glassresistor@gmail.com>
2018-08-17 11:42:49 -0700

    switched to update all for message cleanup migrations [#159839668]

--- db/migrate/20180816222016_cleanup_message_history.rb
+++ db/migrate/20180816222016_cleanup_message_history.rb
@@ -1,8 +1,9 @@
 class CleanupMessageHistory < ActiveRecord::Migration[5.1]
   def up
+    # rubocop:disable Rails/SkipsModelValidations
     past_messages = TextMessage.where(sent: false).where(inbound: false).where('send_at < ?', Time.zone.now - 1.hour)
-    past_messages.where.not(twilio_sid: nil).update(sent: true)
-    past_messages.where(twilio_sid: nil).update(twilio_status: 'undelivered')
-    past_messages.where(twilio_sid: nil).update(sent: true)
+    past_messages.where.not(twilio_sid: nil).update_all(sent: true)
+    past_messages.where(twilio_sid: nil).update_all(twilio_status: 'undelivered', sent: true)
+    # rubocop:enable Rails/SkipsModelValidations
   end
 end
