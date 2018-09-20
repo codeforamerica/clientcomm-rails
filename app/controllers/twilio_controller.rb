85fc0801360fc4d41c76052dcc75091a842391e1
Tomas Apodaca <tomas@codeforamerica.org>
2018-09-20 12:10:50 -0700

    keep message status updates consistent
    don't broadcast or alter relationship if message status isn't set
    
    [#160562073]

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -3,45 +3,44 @@ class TwilioController < ApplicationController
   # validate twilio authenticity using twilio middleware
   # config/application.rb:28
 
+  TWILIO_STATUS_DELIVERED = 'delivered'.freeze
+  TWILIO_STATUS_SENT = 'sent'.freeze
+  TWILIO_STATUS_UNDELIVERED = 'undelivered'.freeze
+  TWILIO_STATUS_FAILED = 'failed'.freeze
+
   def incoming_sms
     IncomingMessageJob.perform_later(params: incoming_message_params.to_h)
 
     head :no_content
   end
 
-  # rubocop:disable Metrics/AbcSize
   def incoming_sms_status
-    Rails.logger.tagged('incoming_sms_status') { Rails.logger.warn "#{params[:SmsSid]} - #{request.headers['X-Request-Start']} ... params[:SmsSid]: #{params[:SmsStatus]}" }
     message = Message.find_by twilio_sid: params[:SmsSid]
     return if message.nil?
 
+    heroku_request_start = request.headers['X-Request-Start']
+
     message.with_lock do
-      request_start = request.headers['X-Request-Start']
-      Rails.logger.tagged('incoming_sms_status') { Rails.logger.warn "#{params[:SmsSid]} - #{request.headers['X-Request-Start']} ... message.last_twilio_update: #{message.last_twilio_update}" }
+      if !message.last_twilio_update || heroku_request_start > message.last_twilio_update
+        message_incoming_status = params[:SmsStatus]
+        message.update!(twilio_status: message_incoming_status, last_twilio_update: heroku_request_start)
 
-      if !message.last_twilio_update || request_start > message.last_twilio_update
-        Rails.logger.tagged('incoming_sms_status') { Rails.logger.warn "#{params[:SmsSid]} - #{request.headers['X-Request-Start']} ... updating message.twilio_status to #{params[:SmsStatus]}" }
-        message.update!(twilio_status: params[:SmsStatus], last_twilio_update: request_start)
-      end
+        MessageBroadcastJob.perform_later(message: message)
 
-      MessageBroadcastJob.perform_later(message: message)
-
-      if params[:SmsStatus] == 'delivered'
-        Rails.logger.tagged('incoming_sms_status') { Rails.logger.warn "#{params[:SmsSid]} - #{request.headers['X-Request-Start']} ... updating reporting_relationship.has_message_error to FALSE because status is #{params[:SmsStatus]}" }
-        message.reporting_relationship.update!(has_message_error: false)
-      elsif ['failed', 'undelivered'].include?(params[:SmsStatus])
-        Rails.logger.tagged('incoming_sms_status') { Rails.logger.warn "#{params[:SmsSid]} - #{request.headers['X-Request-Start']} ... updating reporting_relationship.has_message_error to TRUE because status is #{params[:SmsStatus]}" }
-        message.reporting_relationship.update!(has_message_error: true)
-        analytics_track(
-          label: 'message_send_failed',
-          data: message.analytics_tracker_data
-        )
+        if [TWILIO_STATUS_DELIVERED, TWILIO_STATUS_SENT].include?(message_incoming_status)
+          message.reporting_relationship.update!(has_message_error: false)
+        elsif [TWILIO_STATUS_FAILED, TWILIO_STATUS_UNDELIVERED].include?(message_incoming_status)
+          message.reporting_relationship.update!(has_message_error: true)
+          analytics_track(
+            label: 'message_send_failed',
+            data: message.analytics_tracker_data
+          )
+        end
       end
     end
 
     head :no_content
   end
-  # rubocop:enable Metrics/AbcSize
 
   def incoming_voice
     voice_client = VoiceService.new
