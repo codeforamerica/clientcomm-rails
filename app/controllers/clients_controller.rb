1401b655aa26d2628fcac92f171c36155b8dca3e
Tomas Apodaca <tomas@codeforamerica.org>
2018-09-25 16:54:13 -0700

    added phone number data parameter to form
    experimenting with rendering the name and phone number checkboxes
    
    [#157300281]

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -250,7 +250,13 @@ class ClientsController < ApplicationController
 
   def merge_clients
     current_user.clients.active.where.not(id: @client.id).order(:first_name, :last_name)
-                .map { |c| ["#{c.first_name.strip} #{c.last_name.strip}", c.id] }
+                .map do |c|
+                  [
+                    "#{c.first_name.strip} #{c.last_name.strip}",
+                    c.id,
+                    { 'data-phone-number' => PhoneNumberParser.format_for_display(c.phone_number) }
+                  ]
+                end
   end
 
   def client_params
