e51bcb7145f4d0fee3ae0d5dab600c724dc65f1f
Tomas Apodaca <tomas@codeforamerica.org>
2018-10-01 17:17:52 -0700

    implement conversation ends and merge markers
    [#157300281]

--- app/controllers/merge_reporting_relationships_controller.rb
+++ app/controllers/merge_reporting_relationships_controller.rb
@@ -12,28 +12,45 @@ class MergeReportingRelationshipsController < ApplicationController
       client: Client.find(merge_params[:selected_client_id])
     )
 
-    chosen_phone_number_client_id = merge_params[:merge_clients][:phone_number]
+    chosen_phone_number_client_id = merge_params[:merge_clients][:phone_number].to_i
     rr_to, rr_from = if chosen_phone_number_client_id == rr_current.client.id
                        [rr_current, rr_selected]
                      else
                        [rr_selected, rr_current]
                      end
 
-    chosen_full_name_client_id = merge_params[:merge_clients][:full_name]
+    to_full_name = rr_to.client.full_name
+    from_full_name = rr_from.client.full_name
+
+    chosen_full_name_client_id = merge_params[:merge_clients][:full_name].to_i
     if chosen_full_name_client_id == rr_from.client.id
       rr_to.client.update!(first_name: rr_from.client.first_name, last_name: rr_from.client.last_name)
       NotificationSender.notify_users_of_changes(user: current_user, client: rr_to.client)
     end
 
-    # add 'conversation ends' marker
+    Message.create_conversation_ends_marker(
+      reporting_relationship: rr_from,
+      full_name: from_full_name,
+      phone_number: rr_from.client.display_phone_number
+    )
 
     rr_from.messages.each do |message|
       message.update!(reporting_relationship: rr_to)
+      # TODO: account for like messages!
+      # TODO: set unread on new rr, user to match messages status
     end
+
+    # TODO: merge category, status, notes on reporting relationships (prefer values on rr_to in a conflict)
 
     rr_from.update!(active: false)
 
-    # add 'merged with' marker
+    Message.create_merged_with_marker(
+      reporting_relationship: rr_to,
+      from_full_name: from_full_name,
+      to_full_name: to_full_name,
+      from_phone_number: rr_from.client.display_phone_number,
+      to_phone_number: rr_to.client.display_phone_number
+    )
 
     # catch errors; if anything doesn't work; redirect to edit form and flash error message
 
--- spec/features/user_merges_clients_spec.rb
+++ spec/features/user_merges_clients_spec.rb
@@ -2,20 +2,20 @@ require 'rails_helper'
 
 feature 'User merges clients', :js do
   let(:user_mine) { create :user, full_name: 'Debra Terrones' }
-  let(:phone_number_source) { '+14155555550' }
-  let(:phone_number_source_display) { '(415) 555-5550' }
-  let(:phone_number_second) { '+14155555551' }
-  let(:phone_number_second_display) { '(415) 555-5551' }
-  let(:phone_number_target) { '+14155555552' }
-  let(:phone_number_target_display) { '(415) 555-5552' }
-  let(:first_name_source) { 'Feaven X.' }
-  let(:first_name_second) { 'F.' }
-  let(:first_name_target) { 'Feaven' }
-  let(:last_name_source) { 'Girma' }
-  let(:last_name_second) { 'G.' }
-  let(:last_name_target) { 'Girma' }
-  let!(:client_target) { create :client, user: user_mine, phone_number: phone_number_target, first_name: first_name_target, last_name: last_name_target }
-  let(:rr_target) { ReportingRelationship.find_by(user: user_mine, client: client_target) }
+  let(:phone_number_from) { '+14155555550' }
+  let(:phone_number_from_display) { '(415) 555-5550' }
+  let(:phone_number_other) { '+14155555551' }
+  let(:phone_number_other_display) { '(415) 555-5551' }
+  let(:phone_number_to) { '+14155555552' }
+  let(:phone_number_to_display) { '(415) 555-5552' }
+  let(:first_name_from) { 'Feaven X.' }
+  let(:first_name_other) { 'F.' }
+  let(:first_name_to) { 'Feaven' }
+  let(:last_name_from) { 'Girma' }
+  let(:last_name_other) { 'G.' }
+  let(:last_name_to) { 'Girma' }
+  let!(:client_to) { create :client, user: user_mine, phone_number: phone_number_to, first_name: first_name_to, last_name: last_name_to }
+  let(:rr_to) { ReportingRelationship.find_by(user: user_mine, client: client_to) }
 
   before do
     FeatureFlag.create!(flag: 'court_dates', enabled: true)
@@ -25,71 +25,77 @@ feature 'User merges clients', :js do
 
   context 'the user only has one client' do
     scenario 'the merge form does not appear' do
-      within "tr#client_#{client_target.id}" do
+      within "tr#client_#{client_to.id}" do
         find('td.next-court-date-at', text: '--').click
       end
 
-      expect(page).to have_current_path(edit_client_path(client_target))
+      expect(page).to have_current_path(edit_client_path(client_to))
       expect(page).to_not have_content('Merge duplicate clients')
     end
   end
 
   context 'the user has more than one client' do
-    let!(:client_second) { create :client, user: user_mine, phone_number: phone_number_second, first_name: first_name_second, last_name: last_name_second }
-    let!(:client_source) { create :client, user: user_mine, phone_number: phone_number_source, first_name: first_name_source, last_name: last_name_source }
+    let!(:client_other) { create :client, user: user_mine, phone_number: phone_number_other, first_name: first_name_other, last_name: last_name_other }
+    let!(:client_from) { create :client, user: user_mine, phone_number: phone_number_from, first_name: first_name_from, last_name: last_name_from }
+    let(:rr_from) { ReportingRelationship.find_by(user: user_mine, client: client_from) }
+
+    before do
+      create_list :text_message, 5, reporting_relationship: rr_to
+      create_list :text_message, 5, reporting_relationship: rr_from
+    end
 
     scenario 'successfully merges two clients' do
-      step 'navigates to target client edit form' do
-        within "tr#client_#{client_target.id}" do
+      step 'navigates to "to" client edit form' do
+        within "tr#client_#{client_to.id}" do
           find('td.next-court-date-at', text: '--').click
         end
 
-        expect(page).to have_current_path(edit_client_path(client_target))
+        expect(page).to have_current_path(edit_client_path(client_to))
         expect(page).to have_content('Merge duplicate clients')
         expect(page).to_not have_content('Choose a name')
         expect(page).to_not have_content('Choose a phone number')
       end
 
-      step 'selects second client' do
-        find('#merge_reporting_relationship_selected_client_id').find(:option, client_second.full_name).select_option
+      step 'selects "other" client' do
+        find('#merge_reporting_relationship_selected_client_id').find(:option, client_other.full_name).select_option
         expect(page).to have_content('Choose a name')
-        expect(page).to have_content(client_target.full_name)
-        expect(page).to have_content(client_second.full_name)
+        expect(page).to have_content(client_to.full_name)
+        expect(page).to have_content(client_other.full_name)
         expect(page).to have_content('Choose a phone number')
-        expect(page).to have_content(phone_number_target_display)
-        expect(page).to have_content(phone_number_second_display)
+        expect(page).to have_content(phone_number_to_display)
+        expect(page).to have_content(phone_number_other_display)
         expect(page).to have_button('Merge', disabled: true)
       end
 
       step 'chooses preferred phone number and name' do
-        choose client_second.full_name
-        choose phone_number_target_display
+        choose client_other.full_name
+        choose phone_number_to_display
         expect(page).to have_button('Merge', disabled: false)
       end
 
-      step 'changes mind and selects source client' do
-        find('#merge_reporting_relationship_selected_client_id').find(:option, client_source.full_name).select_option
+      step 'changes mind and selects "from" client' do
+        find('#merge_reporting_relationship_selected_client_id').find(:option, client_from.full_name).select_option
         expect(page).to have_content('Choose a name')
-        expect(page).to have_content(client_target.full_name)
-        expect(page).to have_content(client_source.full_name)
+        expect(page).to have_content(client_to.full_name)
+        expect(page).to have_content(client_from.full_name)
         expect(page).to have_content('Choose a phone number')
-        expect(page).to have_content(phone_number_target_display)
-        expect(page).to have_content(phone_number_source_display)
+        expect(page).to have_content(phone_number_to_display)
+        expect(page).to have_content(phone_number_from_display)
         expect(page).to have_button('Merge', disabled: true)
       end
 
       step 'chooses preferred phone number and name and submits the form' do
-        choose client_source.full_name
-        choose phone_number_target_display
+        choose client_from.full_name
+        choose phone_number_to_display
         expect(page).to have_button('Merge', disabled: false)
       end
 
       step 'submits the merge form' do
         click_on 'Merge'
-        expect(page).to have_current_path(reporting_relationship_path(rr_target))
-        expect(page).to have_content client_source.full_name
-        expect(page).to have_content "\"#{client_source.full_name} #{phone_number_source_display}\" conversation ends"
-        expect(page).to have_content "\"#{client_source.full_name} #{phone_number_source_display}\" merged with \"#{client_target.full_name} #{phone_number_target_display}\""
+        expect(page).to have_current_path(reporting_relationship_path(rr_to))
+        expect(page).to have_content client_from.full_name
+        expect(page).to have_content "\"#{client_from.full_name} #{phone_number_from_display}\" conversation ends"
+        expect(page).to have_content "\"#{client_from.full_name} #{phone_number_from_display}\" merged with \"#{client_to.full_name} #{phone_number_to_display}\""
         expect(page).to have_css '.flash__message', text: I18n.t('flash.notices.merge')
       end
     end
